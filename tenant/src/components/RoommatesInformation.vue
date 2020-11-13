ac10d063ea4718ebc951f2c23272962908e319f5
Matthieu Bollot <m.bollot@code-troopers.com>
2020-11-13 18:21:41 +0100

    feat(profile): add radio button on step 2

---
+++ tenant/src/components/RoommatesInformation.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="rf-grid-row rf-grid-row--center">
+    <div class="col-md-8 col-lg-6">
+      <form name="form" @submit.prevent="handleUpdateMailsInformation">
+        <div class="rf-grid-row rf-grid-row--center">
+          <div class="rf-col-12 rf-margin-bottom-3N">
+            <validation-provider rules="required" v-slot="{ errors }">
+              <div
+                class="rf-input-group"
+                :class="errors[0] ? 'rf-input-group--error' : ''"
+              >
+                <label class="rf-label" for="email">Email :</label>
+                <input
+                  v-model="email"
+                  class="form-control validate-required rf-input"
+                  id="email"
+                  name="email"
+                  placeholder="Ex : exemple@exemple.fr"
+                  type="email"
+                />
+                <span class="rf-error-text" v-if="errors[0]">{{
+                  errors[0]
+                }}</span>
+              </div>
+            </validation-provider>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { ValidationObserver, ValidationProvider } from "vee-validate";
+import { extend } from "vee-validate";
+import { required, email } from "vee-validate/dist/rules";
+
+extend("email", {
+  ...email,
+  message: "email non valide"
+});
+
+extend("required", {
+  ...required,
+  message: "Ce champ est requis"
+});
+
+@Component({
+  components: {
+    ValidationProvider,
+    ValidationObserver
+  }
+})
+export default class RoommatesInformation extends Vue {
+  @Prop({ default: false }) private couple?: boolean;
+  email = "";
+
+  handleUpdateMailsInformation() {
+    // TODO
+  }
+}
+</script>
+
+<style scoped lang="scss"></style>
+
+<i18n>
+{
+"en": {
+},
+"fr": {
+}
+}
+</i18n>
--- tenant/src/components/TenantInformationForm.vue
+++ tenant/src/components/TenantInformationForm.vue
@@ -8,6 +8,50 @@
         })
       }}
     </p>
+
+    <div class="rf-form-group">
+      <fieldset class="rf-fieldset">
+        <div class="rf-fieldset__content">
+          <div class="rf-radio-group">
+            <input
+              type="radio"
+              id="alone"
+              value="alone"
+              v-model="tenantStatus"
+            />
+            <label class="rf-label" for="alone">{{ $t("alone") }}</label>
+          </div>
+          <div class="rf-radio-group">
+            <input
+              type="radio"
+              id="couple"
+              value="couple"
+              v-model="tenantStatus"
+            />
+            <label class="rf-label" for="couple">{{ $t("couple") }}</label>
+          </div>
+          <RoommatesInformation couple="true" v-if="tenantStatus === 'couple'">
+          </RoommatesInformation>
+          <div class="rf-radio-group">
+            <input
+              type="radio"
+              id="roommate"
+              value="roommate"
+              v-model="tenantStatus"
+            />
+            <label class="rf-label" for="roommate">{{ $t("roommate") }}</label>
+          </div>
+          <RoommatesInformation v-if="tenantStatus === 'roommate'">
+          </RoommatesInformation>
+        </div>
+      </fieldset>
+    </div>
+
+    <div class="rf-margin-bottom-5N">
+      <button class="rf-btn" type="submit">
+        {{ $t("confirm") }}
+      </button>
+    </div>
   </div>
 </template>
 
@@ -17,6 +61,7 @@ import { User } from "df-shared/src/models/User";
 import { ValidationObserver, ValidationProvider } from "vee-validate";
 import { extend } from "vee-validate";
 import { required, regex } from "vee-validate/dist/rules";
+import RoommatesInformation from "@/components/RoommatesInformation.vue";
 
 extend("regex", {
   ...regex,
@@ -30,12 +75,14 @@ extend("required", {
 
 @Component({
   components: {
+    RoommatesInformation,
     ValidationProvider,
     ValidationObserver
   }
 })
 export default class TenantInformationForm extends Vue {
   @Prop() private user!: User;
+  tenantStatus = "";
 
   handleNameInformation() {
     this.$store.dispatch("setNames", this.user).then(null, error => {
@@ -54,14 +101,20 @@ export default class TenantInformationForm extends Vue {
 "firstname": "Prénom du locataire",
 "lastname": "Nom du locataire",
 "zipcode": "Code postal",
-"tenantPresentation": "Le locataire sera {firstname} {lastname}"
+"tenantPresentation": "Le locataire sera {firstname} {lastname}. Vous désirez louer un logement :",
+"alone": "Seul",
+"couple": "En couple",
+"roommate": "En colocation"
 },
 "fr": {
 "confirm": "Confirmer",
 "firstname": "Prénom du locataire",
 "lastname": "Nom du locataire",
 "zipcode": "Code postal",
-"tenantPresentation": "Le locataire sera {firstname} {lastname}"
+"tenantPresentation": "Le locataire sera {firstname} {lastname}. Vous désirez louer un logement :",
+"alone": "Seul",
+"couple": "En couple",
+"roommate": "En colocation"
 }
 }
 </i18n>
