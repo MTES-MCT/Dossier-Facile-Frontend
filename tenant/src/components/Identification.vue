9f85967cadd91f7b72268aae99fb4a1b60e7b6b3
Matthieu Bollot <m.bollot@code-troopers.com>
2020-11-18 23:58:27 +0100

    feat(documents): add select on identification page

--- tenant/src/components/Identification.vue
+++ tenant/src/components/Identification.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
-    <h1>
-      Identification
-    </h1>
+    <div>
+      <label class="rf-label" for="select">
+        J'ajoute une pièce d'identité en cours de validité. Attention, veillez à
+        ajouter votre pièce recto-verso !
+      </label>
+      <select class="rf-select" id="select" name="select">
+        <option value="" selected disabled hidden>- Select -</option>
+        <option value="FRENCH_IDENTITY_CARD">{{ $t("cni") }}</option>
+        <option value="FRENCH_PASSPORT">{{ $t("passport") }}</option>
+        <option value="FRENCH_RESIDENCE_PERMIT">{{ $t("permit") }}</option>
+        <option value="OTHER_IDENTIFICATION">{{ $t("other") }}</option>
+      </select>
+    </div>
   </div>
 </template>
 
@@ -10,8 +20,7 @@
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class Identification extends Vue {
-}
+export default class Identification extends Vue {}
 </script>
 
 <style scoped lang="scss"></style>
@@ -19,8 +28,16 @@ export default class Identification extends Vue {
 <i18n>
 {
 "en": {
+"cni": "Carte nationale d'identité",
+"passport": "Passeport",
+"permit": "Permis de conduire",
+"other": "Autre"
 },
 "fr": {
+"cni": "Carte nationale d'identité",
+"passport": "Passeport",
+"permit": "Permis de conduire",
+"other": "Autre"
 }
 }
 </i18n>
