32062f46b4eefaa4265c056d43d70b5dc206fe77
Matthieu Bollot <m.bollot@code-troopers.com>
2020-11-19 16:05:49 +0100

    feat(documents): prepare residency component, add select

--- tenant/src/components/Residency.vue
+++ tenant/src/components/Residency.vue
@@ -1,15 +1,40 @@
 <template>
   <div>
-    <h1>
-      Residency
-    </h1>
+    <div>
+      <label class="rf-label" for="select">
+        Votre situation d'hébergement actuelle :
+      </label>
+      <select
+        v-model="user.residency"
+        class="rf-select"
+        id="select"
+        name="select"
+      >
+        <option value="" selected disabled hidden>- Select -</option>
+        <option value="TENANT">{{ $t("tenant") }}</option>
+        <option value="OWNER">{{ $t("owner") }}</option>
+        <option value="GUEST">{{ $t("guest") }}</option>
+        <option value="GUEST_PARENTS">{{ $t("guest-parents") }}</option>
+      </select>
+    </div>
+    <div>
+      J'ajoute un avis de taxes foncières de moins d'un an.
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import { mapState } from "vuex";
 
-@Component
+@Component({
+  computed: {
+    ...mapState({
+      user: "user",
+      currentStep: "currentStep"
+    })
+  }
+})
 export default class Residency extends Vue {}
 </script>
 
@@ -18,8 +43,16 @@ export default class Residency extends Vue {}
 <i18n>
 {
 "en": {
+"tenant": "Locataire",
+"owner": "Propriétaire",
+"guest": "Invité",
+"guest-parents": "Chez vos parents"
 },
 "fr": {
+"tenant": "Locataire",
+"owner": "Propriétaire",
+"guest": "Invité",
+"guest-parents": "Chez vos parents"
 }
 }
 </i18n>
