eccc7be6536449880d4b216159ca387d3fe22164
Matthieu Bollot <m.bollot@code-troopers.com>
2020-11-24 23:36:41 +0100

    feat(EditSummary): improve card content

--- tenant/src/components/EditSummary.vue
+++ tenant/src/components/EditSummary.vue
@@ -1,28 +1,95 @@
 <template>
   <div>
-    <Card :expandable="false" :expanded="true">
-      <template v-slot:header>
-        {{ $t("title") }}
-      </template>
-      <template v-slot:body>
+    <NakedCard>
+      <template v-slot:content>
         <div>
-          Prénom et nom
+          <a href="#" class="rf-link">
+            {{ $t("title") }}
+          </a>
+          <hr />
+          <div class="rf-card__desc">
+            <section>
+              <div class="row" v-if="user.lastName">
+                <div>
+                  <div class="subtitle">Prénom et Nom</div>
+                  {{ user.firstName }} {{ user.lastName }}
+                </div>
+                <div class="edit-step-btn" @click="setStep(0)">edit</div>
+              </div>
+              <div class="row" v-if="user.tenantType">
+                <div>
+                  <div class="subtitle">Type de location</div>
+                  {{ $t(user.tenantType) }}
+                </div>
+                <div class="edit-step-btn" @click="setStep(1)">edit</div>
+              </div>
+            </section>
+          </div>
         </div>
-        <div @click="setStep(0)">edit</div>
         <div>
-          Type de location
+          <a href="#" class="rf-link">
+            {{ $t("second-title") }}
+          </a>
+          <hr />
+          <div class="rf-card__desc">
+            <section>
+              <div class="row" v-if="user.identification">
+                <div class="subtitle">Pièce d'identité</div>
+                <div class="row">
+                  <div class="edit-step-btn" @click="setStep(2)">watch</div>
+                  <div class="edit-step-btn" @click="setStep(2)">edit</div>
+                </div>
+              </div>
+              <div class="row" v-if="user.residency">
+                <div class="subtitle">Justificatif de domicile</div>
+                <div class="row">
+                  <div class="edit-step-btn" @click="setStep(3)">watch</div>
+                  <div class="edit-step-btn" @click="setStep(3)">edit</div>
+                </div>
+              </div>
+              <div class="row" v-if="user.professional">
+                <div class="subtitle">
+                  Justificatif de situation professionelle
+                </div>
+                <div class="row">
+                  <div class="edit-step-btn" @click="setStep(4)">watch</div>
+                  <div class="edit-step-btn" @click="setStep(4)">edit</div>
+                </div>
+              </div>
+              <div class="row" v-if="user.financial">
+                <div class="subtitle">Justificatif de revenu</div>
+                <div class="row">
+                  <div class="edit-step-btn" @click="setStep(5)">watch</div>
+                  <div class="edit-step-btn" @click="setStep(5)">edit</div>
+                </div>
+              </div>
+              <div class="row" v-if="user.tax">
+                <div class="subtitle">Avis d'imposition</div>
+                <div class="row">
+                  <div class="edit-step-btn" @click="setStep(6)">watch</div>
+                  <div class="edit-step-btn" @click="setStep(6)">edit</div>
+                </div>
+              </div>
+            </section>
+          </div>
         </div>
-        <div @click="setStep(1)">edit</div>
       </template>
-    </Card>
+    </NakedCard>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import Card from "@/components/Card.vue";
+import { mapState } from "vuex";
+import NakedCard from "@/components/NakedCard.vue";
 @Component({
-  components: { Card }
+  components: { NakedCard },
+  computed: {
+    ...mapState({
+      user: "user",
+      currentStep: "currentStep"
+    })
+  }
 })
 export default class EditSummary extends Vue {
   setStep(n: number) {
@@ -31,16 +98,39 @@ export default class EditSummary extends Vue {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.row {
+  display: flex;
+  justify-content: space-between;
+  padding: 0.2rem;
+  div {
+    align-self: center;
+  }
+}
+.edit-step-btn {
+  align-self: center;
+  padding: 0.2rem;
+}
+.subtitle {
+  font-weight: bold;
+}
+</style>
 
 <i18n>
 {
 "en": {
-"title": "Informations du locataire"
+"title": "Information du locataire",
+"second-title": "Documents",
+"ALONE": "Seul",
+"COUPLE": "En couple",
+"CREATE": "En colocation"
 },
 "fr": {
-"title": "Informations du locataire"
+"title": "Information du locataire",
+"second-title": "Documents",
+"ALONE": "Seul",
+"COUPLE": "En couple",
+"CREATE": "En colocation"
 }
 }
 </i18n>
---
+++ tenant/src/components/NakedCard.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="card">
+    <slot name="content"></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Card extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.hidden {
+  display: none;
+}
+.card {
+  padding: 10px;
+  margin-top: 30px;
+  margin-bottom: 20px;
+  max-width: 500px;
+
+  -webkit-box-shadow: 0 7px 15px 0 rgba(0, 0, 0, 0.5);
+  -moz-box-shadow: 0 7px 15px 0 rgba(0, 0, 0, 0.5);
+  box-shadow: 0 7px 15px 0 rgba(0, 0, 0, 0.5);
+
+  a {
+    &:before {
+      content: none;
+    }
+  }
+}
+
+.rf-link {
+  flex-direction: row;
+}
+
+hr {
+  color: gray;
+  border-style: inset;
+  border-width: 1px;
+  margin: 0.5em auto;
+}
+</style>
