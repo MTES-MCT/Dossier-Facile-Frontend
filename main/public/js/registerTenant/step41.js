f79d5d4d6e8461f669feb2d19c81b82ff4b0ddb6
Matthieu Bollot <m.bollot@code-troopers.com>
2020-12-02 17:50:19 +0100

    feat(blog): Fix faq page due to new css

--- main/public/js/registerTenant/step41.js
+++
@@ -1,7 +0,0 @@
-$('#tenantSituation').on('change', function () {
-    if (this.value === '3' || this.value === '4') {
-        var modal = $('#tenantSituationWarning');
-        mr.modals.showModal(modal);
-        // $('.modal-file').modal('show');
-    }
-});
