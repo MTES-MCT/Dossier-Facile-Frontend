376cd951eb5efcb38e559147853786f9851ca53d
Matthieu Bollot <m.bollot@code-troopers.com>
2020-12-07 18:19:49 +0100

    feat(i18n): use navigator language and default fr

--- tenant/src/i18n.ts
+++ tenant/src/i18n.ts
@@ -21,7 +21,7 @@ function loadLocaleMessages(): LocaleMessages {
 }
 
 export default new VueI18n({
-  locale: process.env.VUE_APP_I18N_LOCALE || "en",
-  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || "en",
+  locale: navigator.language || "fr",
+  fallbackLocale: navigator.language || "fr",
   messages: loadLocaleMessages()
 });
