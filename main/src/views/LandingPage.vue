6aaa204b6a3a7731a6648e134f5cfbae279baa20
Matthieu Bollot <m.bollot@code-troopers.com>
2020-12-09 22:49:19 +0100

    feat(landing): update text and style

--- main/src/views/LandingPage.vue
+++ main/src/views/LandingPage.vue
@@ -58,7 +58,14 @@
         <div class="rf-grid-row">
           <div class="rf-col-md-6 rf-grid-col">
             <h4 class="color--secondary rf-mb-0">{{ $t("s2.h1") }}</h4>
-            <p>{{ $t("s2.p1a") }}<br>{{ $t("s2.p1b") }}</p>
+            <p>{{ $t("s2.p1") }}</p>
+            <ul>
+              <li>{{ $t("s2.u1a") }}</li>
+              <li>{{ $t("s2.u1b") }}</li>
+              <li>{{ $t("s2.u1c") }}</li>
+              <li>{{ $t("s2.u1d") }}</li>
+              <li>{{ $t("s2.u1e") }}</li>
+            </ul>
 
             <h4 class="color--secondary rf-mb-0">{{ $t("s2.h2") }}</h4>
             <p>{{ $t("s2.p2") }}</p>
@@ -70,7 +77,7 @@
             <img src="../assets/icons/justif.svg" />
           </div>
         </div>
-        <router-link class="rf-btn" to="/tenant">
+        <router-link class="rf-btn rf-mt-3w" to="/tenant">
           {{ $t("s2.button") }}
         </router-link>
       </div>
@@ -83,6 +90,9 @@
             {{ $t("s3.text1") }}
             <br>
             {{ $t("s3.text2") }}
+          </p>
+          <p>
+            {{ $t("s3.text3") }}
           </p>
 <!--           <router-link to="/tenant">
             {{ $t("s3.link") }}
@@ -183,6 +193,18 @@ blockquote {
   }
 }
 
+ul li {
+  display: flex;
+  margin-bottom: 0;
+}
+
+ul li:before {
+  content: "\2713";
+  display: inline-block;
+  color: limegreen;
+  font-size: 1rem;
+}
+
 </style>
 
 <i18n>
@@ -190,38 +212,43 @@ blockquote {
   "en": {
     "s0": {
       "title": "Montez un dossier de location en béton pour trouver le logement de vos rêves",
-      "text": "DossierFacile est un service public gratuit et sécurisé qui vous aide à constituer un dossier de location numérique de qualité pour mettre toutes les chances de votre côté.",
+      "text": "DossierFacile vous aide à constituer un dossier de location numérique de qualité pour mettre toutes les chances de votre côté.",
       "link": "Monter votre dossier de location"
     },
     "s1": {
       "h1": "Démarquez-vous grâce à un dossier de qualité",
       "p1": "Transmettez aux propriétaires un dossier clair, complet et cohérent pour maximiser vos chances.",
       "h2": "Soyez le premier à répondre à une annonce",
-      "p2": "Soyez le plus réactif en envoyant en un clic un dossier numérique complet avec toutes les pièces jointes nécessaires.",
+      "p2": "Soyez le plus réactif en envoyant en un clic votre dossier numérique.",
       "h3": "Rassurez votre futur·e propriétaire",
-      "p3": "Certifiez la qualité et la véracité de votre dossier de location grâce au label DossierFacile."
+      "p3": "Après une batterie de tests par nos opérateurs, votre dossier est labellisé par l'État."
     },
     "s2": {
-      "title": "Constituez votre dossier de location en quelques clics",
+      "title": "Constituez votre dossier de location en trois étapes",
       "h1": "Je télécharge mes pièces",
-      "p1a": "Pièce d'identité, avis d'imposition, fiches de paie…",
-      "p1b": "DossierFacile vous guide pour ne rien oublier.",
+      "p1": "DossierFacile vous demande les pièces nécessaires et rien que celles-là !",
+      "u1a": "Pièce d'identité",
+      "u1b": "Justificatif de domicile",
+      "u1c": "Justificatif de situation professionnelle",
+      "u1d": "Avis d'imposition",
+      "u1e": "Justificatif de ressources",
       "h2": "DossierFacile valide mon dossier",
-      "p2": "Nos agents vérifient votre dossier et vous aident à corriger le vôtre si besoin.",
+      "p2": "Nos agents vérifient votre dossier et vous aident à le corriger si besoin.",
       "h3": "Mon dossier est prêt à être envoyé",
-      "p3": "Plus besoin de refaire un dossier à chaque candidature : votre dossier sécurisé est disponible au format PDF ou en ligne pour être renvoyé aux propriétaires.",
-      "button": "Créer mon dossier de location"
+      "p3": "Votre dossier sécurisé sous forme de lien URL ou au format PDF est prêt à être envoyé aux propriétaires.",
+      "button": "Monter mon dossier de location"
     },
     "s3": {
       "title": "Protégez vos informations personnelles",
-      "text1": "Avec DossierFacile, plus de papiers d'identité éparpillés dans la nature : votre dossier contient les pièces autorisées par la loi et rien de plus !",
-      "text2": "Vos pièces d'identité sont protégées par des filigranes pour vous protéger contre la fraude de propriétaires peu scrupuleux.",
-      "link": "En savoir plus sar la sécurité de DossierFacile"
+      "text1": "Avec DossierFacile, plus de papiers d'identité éparpillés dans la nature !",
+      "text2": "Vos documents sont recouverts de filigranes pour vous protéger contre la fraude de propriétaires peu scrupuleux.",
+      "text3": "Vous pouvez paramétrer l'ouverture de votre lien dossier : vous pouvez le supprimer à tout moment !",
+      "link": "En savoir plus sur la sécurité des données chez DossierFacile"
     },
     "s4": {
       "title1": "Déjà",
-      "title2": "9000",
-      "title3": "locataires heureux grâce à DossierFacile",
+      "title2": "65 000",
+      "title3": "Dossiers labellisés grâce à DossierFacile",
       "t1": "Le dossier de Karim s'est tout de suite démarqué : c'était le seul dossier reçu complet !",
       "author1": "Joseph, propriétaire à Nantes",
       "t2": "Simple, efficace et pratique : ça m'a fait gagner beaucoup de temps dans ma recherche d'appartement.",
@@ -234,36 +261,43 @@ blockquote {
   "fr": {
     "s0": {
       "title": "Montez un dossier de location en béton pour trouver le logement de vos rêves",
-      "text": "DossierFacile est un service public gratuit et sécurisé qui vous aide à constituer un dossier de location numérique de qualité pour mettre toutes les chances de votre côté.",
+      "text": "DossierFacile vous aide à constituer un dossier de location numérique de qualité pour mettre toutes les chances de votre côté.",
       "link": "Monter votre dossier de location"
     },
     "s1": {
       "h1": "Démarquez-vous grâce à un dossier de qualité",
       "p1": "Transmettez aux propriétaires un dossier clair, complet et cohérent pour maximiser vos chances.",
       "h2": "Soyez le premier à répondre à une annonce",
-      "p2": "Soyez le plus réactif en envoyant en un clic un dossier numérique complet avec toutes les pièces jointes nécessaires.",
+      "p2": "Soyez le plus réactif en envoyant en un clic votre dossier numérique.",
       "h3": "Rassurez votre futur·e propriétaire",
-      "p3": "Certifiez la qualité et la véracité de votre dossier de location grâce au label DossierFacile."
+      "p3": "Après une batterie de tests par nos opérateurs, votre dossier est labellisé par l'État."
     },
     "s2": {
-      "title": "Constituez votre dossier de location en quelques clics",
+      "title": "Constituez votre dossier de location en trois étapes",
       "h1": "Je télécharge mes pièces",
-      "p1a": "Pièce d'identité, avis d'imposition, fiches de paie…",
-      "p1b": "DossierFacile vous guide pour ne rien oublier.",
+      "p1": "DossierFacile vous demande les pièces nécessaires et rien que celles-là !",
+      "u1a": "Pièce d'identité",
+      "u1b": "Justificatif de domicile",
+      "u1c": "Justificatif de situation professionnelle",
+      "u1d": "Avis d'imposition",
+      "u1e": "Justificatif de ressources",
       "h2": "DossierFacile valide mon dossier",
-      "p2": "Nos agents vérifient votre dossier et vous aident à corriger le vôtre si besoin.",
+      "p2": "Nos agents vérifient votre dossier et vous aident à le corriger si besoin.",
       "h3": "Mon dossier est prêt à être envoyé",
-      "p3": "Plus besoin de refaire un dossier à chaque candidature : votre dossier sécurisé est disponible au format PDF ou en ligne pour être renvoyé aux propriétaires.",
-      "button": "Créer mon dossier de location"
+      "p3": "Votre dossier sécurisé sous forme de lien URL ou au format PDF est prêt à être envoyé aux propriétaires.",
+      "button": "Monter mon dossier de location"
     },
     "s3": {
       "title": "Protégez vos informations personnelles",
-      "text1": "Avec DossierFacile, plus de papiers d'identité éparpillés dans la nature : votre dossier contient les pièces autorisées par la loi et rien de plus !",
-      "text2": "Vos pièces d'identité sont protégées par des filigranes pour vous protéger contre la fraude de propriétaires peu scrupuleux.",
-      "link": "En savoir plus sar la sécurité de DossierFacile"
+      "text1": "Avec DossierFacile, plus de papiers d'identité éparpillés dans la nature !",
+      "text2": "Vos documents sont recouverts de filigranes pour vous protéger contre la fraude de propriétaires peu scrupuleux.",
+      "text3": "Vous pouvez paramétrer l'ouverture de votre lien dossier : vous pouvez le supprimer à tout moment !",
+      "link": "En savoir plus sur la sécurité des données chez DossierFacile"
     },
     "s4": {
-      "title": "Déjà 9000 locataires heureux grâce à DossierFacile",
+      "title1": "Déjà",
+      "title2": "65 000",
+      "title3": "Dossiers labellisés grâce à DossierFacile",
       "t1": "Le dossier de Karim s'est tout de suite démarqué : c'était le seul dossier reçu complet !",
       "author1": "Joseph, propriétaire à Nantes",
       "t2": "Simple, efficace et pratique : ça m'a fait gagner beaucoup de temps dans ma recherche d'appartement.",
