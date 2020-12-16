3ba2b39b38e6c4cf23b9c5dca2ab68117e7f0854
Matthieu Bollot <m.bollot@code-troopers.com>
2020-12-16 23:43:16 +0100

    feat(signup): Add link to login for existing account

--- tenant/src/views/SignupPage.vue
+++ tenant/src/views/SignupPage.vue
@@ -2,6 +2,11 @@
   <div class="rf-container">
     <div class="signup">
       <Register @on-register="onRegister" />
+      <div class="text-center">
+        <router-link to="/login">
+          {{ $t("existing-account") }}
+        </router-link>
+      </div>
       <Modal v-show="isValidModalVisible" @close="closeModal">
         <template v-slot:body>
           <div class="rf-container">
@@ -55,3 +60,14 @@ export default class SignupPage extends Vue {
   }
 }
 </script>
+
+<i18n>
+{
+"en": {
+    "existing-account": "J'ai déjà un compte"
+},
+"fr": {
+    "existing-account": "J'ai déjà un compte"
+}
+}
+</i18n>
