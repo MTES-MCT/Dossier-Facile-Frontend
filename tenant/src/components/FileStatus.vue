e01d5818efab40b3ea15ac37804d5d9641783600
Matthieu Bollot <m.bollot@code-troopers.com>
2021-01-02 00:03:22 +0100

    feat(file): Add FileStatus page

---
+++ tenant/src/components/FileStatus.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>{{ $t("title") }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { ValidationProvider } from "vee-validate";
+import { mapState } from "vuex";
+import { User } from "df-shared/src/models/User";
+
+@Component({
+  components: { ValidationProvider },
+  computed: {
+    ...mapState({
+      user: "user",
+    }),
+  },
+})
+export default class FileStatus extends Vue {
+  user!: User;
+
+}
+</script>
+
+<i18n>
+{
+  "en": {
+      "title": "Mon dossier"
+  },
+  "fr": {
+      "title": "Mon dossier"
+  }
+}
+</i18n>
