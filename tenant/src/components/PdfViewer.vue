42947cc0e684a88627fc47377781ce56aedad779
Matthieu Bollot <m.bollot@code-troopers.com>
2021-01-08 18:27:27 +0100

    feat(documents): Increase pdf viewer style

--- tenant/src/components/PdfViewer.vue
+++ tenant/src/components/PdfViewer.vue
@@ -1,6 +1,38 @@
 <template>
   <div class="container">
-    <pdf :src="src" style="display: inline-block; width: 100%"></pdf>
+    <div class="rf-grid-col main-content">
+      <div class="rf-mb-3w">
+        <div class="rf-grid-row">
+          <input type="checkbox" v-model="show" />
+          <input v-model.number="page" type="number" style="width: 5em" /> /{{
+            numPages
+          }}
+          <button @click="decreasePage()">«</button>
+          <button @click="increasePage()">»</button>
+        </div>
+      </div>
+      <div class="pdf-content">
+        <div
+          v-if="loadedRatio > 0 && loadedRatio < 1"
+          style="background-color: green; color: white; text-align: center"
+          :style="{ width: loadedRatio * 100 + '%' }"
+        >
+          {{ Math.floor(loadedRatio * 100) }}%
+        </div>
+        <pdf
+          v-if="show"
+          ref="pdf"
+          style="border: 1px solid red"
+          :src="src"
+          :page="page"
+          :rotate="rotate"
+          @progress="loadedRatio = $event"
+          @error="error"
+          @num-pages="numPages = $event"
+          @link-clicked="page = $event"
+        ></pdf>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,6 +45,28 @@ import pdf from "vue-pdf";
 })
 export default class PdfViewer extends Vue {
   @Prop() private src!: string;
+
+  show = true;
+  loadedRatio = 0;
+  page = 1;
+  numPages = 0;
+  rotate = 0;
+
+  decreasePage() {
+      if (this.page > 1) {
+        this.page--;
+      }
+  }
+
+  increasePage() {
+      if (this.page < this.numPages) {
+        this.page++;
+      }
+  }
+
+  error(err: any) {
+      console.log(err);
+  }
 }
 </script>
 
@@ -36,5 +90,10 @@ export default class PdfViewer extends Vue {
   .container {
     min-width: 1140px;
   }
+}
+
+.main-content {
+  width: 80%;
+  margin: auto;
 }
 </style>
