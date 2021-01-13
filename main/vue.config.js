25f85925b08439ac7fda994653cfa1ebedf9fd83
Matthieu Bollot <m.bollot@code-troopers.com>
2021-01-13 08:51:28 +0100

    chore: add prerender-spa-plugin to improve seo

--- main/vue.config.js
+++ main/vue.config.js
@@ -1,3 +1,6 @@
+var PrerenderSpaPlugin = require('prerender-spa-plugin')
+var path = require('path')
+
 module.exports = {
   pluginOptions: {
     i18n: {
@@ -6,5 +9,19 @@ module.exports = {
       localeDir: "locales",
       enableInSFC: true
     }
+  },
+  configureWebpack: config => {
+    if (process.env.NODE_ENV !== 'production') return
+
+    return {
+      plugins: [
+        new PrerenderSpaPlugin(
+          // Absolute path to compiled SPA
+          path.resolve(__dirname, 'dist'),
+          // List of routes to prerender
+          [ '/', '/blog'],
+        ),
+      ]
+    }
   }
-};
+}
