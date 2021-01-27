da10f3d1db523e5b78403e9865642323b67463a2
Matthieu Bollot <m.bollot@code-troopers.com>
2021-01-27 17:44:03 +0100

    chore: user data-rf-reset, clean validate rules

--- tenant/src/components/ValidateFile.vue
+++ tenant/src/components/ValidateFile.vue
@@ -14,27 +14,28 @@
       </div>
       <div>
         <validation-provider
-          :rules="{ regex: /^[0-9., ]+$/ }"
           v-slot="{ errors }"
         >
           <div
             class="rf-input-group"
             :class="errors[0] ? 'rf-input-group--error' : ''"
           >
-            <label for="precision" class="rf-label"
-              >{{ $t("precision") }} :</label
-            >
-            <input
-              id="precision"
-              :placeholder="$t('placeholder')"
-              type="text"
-              v-model="precision"
-              name="precision"
-              class="validate-required form-control rf-input"
-            />
-            <span class="rf-error-text" v-if="errors[0]">{{
-              $t(errors[0])
-            }}</span>
+            <p>
+              <label for="precision" class="rf-label"
+                >{{ $t("precision") }} :</label
+              >
+              <input
+                id="precision"
+                :placeholder="$t('placeholder')"
+                type="text"
+                v-model="precision"
+                name="precision"
+                class="validate-required form-control rf-input"
+              />
+              <span class="rf-error-text" v-if="errors[0]">{{
+                $t(errors[0])
+              }}</span>
+            </p>
           </div>
         </validation-provider>
       </div>
@@ -71,27 +72,28 @@
       </div>
       <div>
         <validation-provider
-          :rules="{ regex: /^[0-9., ]+$/ }"
           v-slot="{ errors }"
         >
           <div
             class="rf-input-group"
             :class="errors[0] ? 'rf-input-group--error' : ''"
           >
-            <label for="precision" class="rf-label"
-              >{{ $t("precision") }} :</label
-            >
-            <input
-              id="precision"
-              :placeholder="$t('placeholder')"
-              type="text"
-              v-model="precision"
-              name="precision"
-              class="validate-required form-control rf-input"
-            />
-            <span class="rf-error-text" v-if="errors[0]">{{
-              $t(errors[0])
-            }}</span>
+            <p>
+              <label for="precision" class="rf-label"
+                >{{ $t("precision") }} :</label
+              >
+              <input
+                id="precision"
+                :placeholder="$t('placeholder')"
+                type="text"
+                v-model="precision"
+                name="precision"
+                class="validate-required form-control rf-input"
+              />
+              <span class="rf-error-text" v-if="errors[0]">{{
+                $t(errors[0])
+              }}</span>
+            </p>
           </div>
         </validation-provider>
       </div>
