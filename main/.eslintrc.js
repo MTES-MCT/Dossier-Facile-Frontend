4f03755b459308cce502e14458100ce50316d2e8
Matthieu Bollot <m.bollot@code-troopers.com>
2021-01-27 16:31:04 +0100

    chore: fix build, fix rule name

--- main/.eslintrc.js
+++ main/.eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "vue/no-irregular-whitespace": "off"
+    "no-irregular-whitespace": "off"
   },
   overrides: [
     {
