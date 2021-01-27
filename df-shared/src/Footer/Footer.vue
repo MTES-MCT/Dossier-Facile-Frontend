aeda7fdb49abd2e36a907a0153a99fbc1adc1665
Matthieu Bollot <m.bollot@code-troopers.com>
2021-01-27 16:24:25 +0100

    chore: bump gouvfr DS, adapt footer

--- df-shared/src/Footer/Footer.vue
+++ df-shared/src/Footer/Footer.vue
@@ -1,69 +1,97 @@
 <template>
-  <footer class="rf-footer" role="contentinfo" id="">
+  <footer class="rf-footer" role="contentinfo" id="footer">
     <div class="rf-container">
       <div class="rf-footer__body">
         <div class="rf-footer__brand">
-          <a class="rf-logo" href="#" title="république<br>française">
+          <a class="rf-logo" href="#" title="république française">
             <span class="rf-logo__title">république <br />française</span>
           </a>
           <a href="#">
-            <img class="rf-footer__logo" src="./logo-betagouvfr.svg" />
+            <img class="footer-logo" src="./logo-betagouvfr.svg" />
           </a>
         </div>
         <div class="rf-footer__content">
-          <ul class="rf-footer__content-links links-center rf-grid-row">
+          <ul class="rf-footer__content-list links-center rf-grid-row">
             <li class="rf-col-lg-3 rf-col-6 align-right">
-              <a href="http://legifrance.gouv.fr">legifrance.gouv.fr</a>
+              <a
+                class="rf-footer__content-link"
+                href="https://legifrance.gouv.fr"
+                >legifrance.gouv.fr</a
+              >
             </li>
             <li class="rf-col-lg-3 rf-col-6 align-right">
-              <a href="http://gouvernement.fr">gouvernement.fr</a>
+              <a class="rf-footer__content-link" href="https://gouvernement.fr"
+                >gouvernement.fr</a
+              >
             </li>
             <li class="rf-col-lg-3 rf-col-6 align-right">
-              <a href="http://service-public.fr">service-public.fr</a>
+              <a
+                class="rf-footer__content-link"
+                href="https://service-public.fr"
+                >service-public.fr</a
+              >
             </li>
             <li class="rf-col-lg-3 rf-col-6 align-right">
-              <a href="http://data.gouv.fr">data.gouv.fr</a>
+              <a class="rf-footer__content-link" href="https://data.gouv.fr"
+                >data.gouv.fr</a
+              >
             </li>
           </ul>
         </div>
       </div>
       <div class="rf-footer__bottom">
         <ul class="rf-footer__bottom-list">
-          <li class="rf-col-lg-3 rf-col-6">
-            <a href="https://beta.gouv.fr/">Les startups d'Etat</a>
+          <li class="rf-footer__bottom-item">
+            <a class="rf-footer__bottom-link" href="https://beta.gouv.fr/"
+              >Les startups d'Etat</a
+            >
           </li>
-          <li class="rf-col-lg-3 rf-col-6">
-            <a :href="`${VUE_APP_MAIN_URL}/faq`">
+          <li class="rf-footer__bottom-item">
+            <a class="rf-footer__bottom-link" :href="`${VUE_APP_MAIN_URL}/faq`">
               {{ $t("faq") }}
             </a>
           </li>
-          <li class="rf-col-lg-3 rf-col-6">
-            <a :href="`${VUE_APP_MAIN_URL}/blog`">
+          <li class="rf-footer__bottom-item">
+            <a
+              class="rf-footer__bottom-link"
+              :href="`${VUE_APP_MAIN_URL}/blog`"
+            >
               {{ $t("blog") }}
             </a>
           </li>
-          <li class="rf-col-lg-3 rf-col-6">
-            <a href="https://github.com/MTES-MCT/Dossier-Facile-Frontend">
+          <li class="rf-footer__bottom-item">
+            <a
+              class="rf-footer__bottom-link"
+              href="https://github.com/MTES-MCT/Dossier-Facile-Frontend"
+            >
               {{ $t("sources") }}
             </a>
           </li>
-          <li>
-            <a class="blue--link" href="mailto:contact@dossierfacile.fr">{{
-              $t("contact")
-            }}</a>
+          <li class="rf-footer__bottom-item">
+            <a
+              class="rf-footer__bottom-link blue--link"
+              href="mailto:contact@dossierfacile.fr"
+              >{{ $t("contact") }}</a
+            >
           </li>
           <li class="rf-footer__bottom-item">
             <a class="rf-footer__bottom-link" href="#">{{
               $t("accessibility")
             }}</a>
           </li>
-          <li>
-            <a :href="`${VUE_APP_MAIN_URL}/securite-des-donnees`">
+          <li class="rf-footer__bottom-item">
+            <a
+              class="rf-footer__bottom-link"
+              :href="`${VUE_APP_MAIN_URL}/securite-des-donnees`"
+            >
               {{ $t("legals") }}
             </a>
           </li>
-          <li class="rf-col-lg-3 rf-col-6">
-            <a :href="`${VUE_APP_MAIN_URL}/information`">
+          <li class="rf-footer__bottom-item">
+            <a
+              class="rf-footer__bottom-link"
+              :href="`${VUE_APP_MAIN_URL}/information`"
+            >
               {{ $t("information") }}
             </a>
           </li>
@@ -85,6 +113,17 @@ export default class MyFooter extends Vue {
 
 <style lang="scss" scoped>
 @import "../scss/_variables.scss";
+.blue--link {
+  color: $primary !important;
+}
+.footer-logo {
+  min-width: 100px;
+  max-width: 100%;
+  width: 200px;
+  height: auto;
+  display: block;
+}
+
 .links-center {
   align-self: center;
 }
@@ -93,21 +132,19 @@ export default class MyFooter extends Vue {
   width: 100%;
 }
 
-li {
-  &.rf-col-lg-3 {
+.rf-footer__content-list {
+  margin-bottom: 0;
+}
+
+.align-right {
+  margin-top: 0;
+  @media (min-width: 768px) {
+    text-align: right;
     margin-right: 0;
   }
 }
 
-.blue--link {
-  color: $primary;
-}
-
-@media (min-width: 768px) {
-  .align-right {
-    text-align: right;
-  }
-}
+/*
 
 .rf-footer__logo {
   min-width: 100px;
@@ -116,6 +153,7 @@ li {
 .rf-footer__content-links {
   margin-bottom: 0;
 }
+ */
 </style>
 
 <i18n>
