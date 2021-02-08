95ca77323893eae1e3fc6dcf1d467d57c090a2b8
Matthieu Bollot <m.bollot@code-troopers.com>
2021-02-08 22:58:57 +0100

    feat(i18n): add translation on document help

--- tenant/src/components/helps/DocumentHelp.vue
+++ tenant/src/components/helps/DocumentHelp.vue
@@ -31,8 +31,8 @@ export default class DocumentHelp extends Vue {
 <i18n>
 {
   "en": {
-    "title": "Aide",
-    "paragraph1": "Si votre pièce est trop lourde, nous vous conseillons de scanner votre pièce (avec une application gratuite pour smartphone par exemple).<br> Si cela ne suffit pas, contactez-nous par mail à l’adresse <a href='mailto:contact@dossierfacile.fr'>contact@dossierfacile.fr</a>, nous nous efforcerons de répondre au plus vite !"
+    "title": "Help",
+    "paragraph1": "If your file is too heavy, we recommend that you scan your file (with a free smartphone app for example). <br> If that is not enough, contact us by email at <a href='mailto: contact@dossierfacile.fr'>contact@dossierfacile.fr</a>, we will do our best to respond as quickly as possible!"
   },
   "fr": {
     "title": "Aide",
