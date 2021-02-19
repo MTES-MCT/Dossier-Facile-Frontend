745bc69c31e41d5cf0b51b9dc2d560784d8eb704
Matthieu Bollot <m.bollot@code-troopers.com>
2021-02-19 16:17:20 +0100

    chore: add missing pages file and source

---
+++ tenant/src/views/File.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>file</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class File extends Vue {}
+</script>
---
+++ tenant/src/views/Source.vue
@@ -0,0 +1,11 @@
+
+<template>
+  <div>source</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class Source extends Vue {}
+</script>
