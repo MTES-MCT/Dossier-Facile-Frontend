15ee3374a88089ff37d7d4f91b9e1cd1d9d82fc6
Matthieu Bollot <m.bollot@code-troopers.com>
2021-03-13 14:54:18 +0100

    feat(statusTag): use color

--- df-shared/src/components/StatusTag.vue
+++ df-shared/src/components/StatusTag.vue
@@ -1,5 +1,5 @@
 <template>
-  <p class="rf-tag">
+  <p class="rf-tag" :class="getClass()">
     <span>{{ $t(status) }}</span>
   </p>
 </template>
@@ -10,8 +10,35 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 @Component
 export default class StatusTag extends Vue {
   @Prop({ default: "EMPTY" }) status!: string;
+
+  getClass() {
+    switch (this.status) {
+      case "TO_PROCESS":
+        return "to-process";
+      case "VALIDATED":
+        return "validated";
+      case "DECLINED":
+        return "declined";
+    }
+  }
 }
 </script>
+
+<style scoped lang="scss">
+/* TODO : use var */
+.to-process {
+  color: #000074;
+  background-color: #ededf2;
+}
+.validated {
+  color: #169b62;
+  background-color: #e3ebd3;
+}
+.declined {
+  color: white;
+  background-color: #f07f87;
+}
+</style>
 
 <i18n>
 {
