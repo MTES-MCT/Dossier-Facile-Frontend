8cdf7b18dbbcc681a3c395b451c01d2ae7ea5a95
Matthieu Bollot <m.bollot@code-troopers.com>
2021-04-19 23:05:57 +0200

    feat(stats): improve first chart

--- main/src/views/Statistics.vue
+++ main/src/views/Statistics.vue
@@ -3,19 +3,36 @@
     <h1>{{ $t("title") }}</h1>
     <div class="fr-grid-row">
       <div class="fr-col-8">
-        <h2>Bar Chart I</h2>
+        <h2>Évolution des dossiers créés</h2>
+        /semaine
         <hr />
         <div class="container-chart text-align-left">
-          <div class="chart"></div>
+          <div id="chart1"></div>
         </div>
       </div>
-      <div class="fr-col-4">t</div>
-      <div class="fr-col-8">
-        <div id="chart2"></div>
+      <div class="fr-col-4">
+        2 354 543<br />Dossiers accompagnés avec DossierFacile
       </div>
-      <div class="fr-col-4"></div>
-      <div class="fr-col-8"></div>
-      <div class="fr-col-4"></div>
+      <div class="fr-col-8">
+        <h2>Évolution des liens partagés</h2>
+        /semaine
+        <hr />
+        <div class="container-chart text-align-left">
+          <div id="chart2"></div>
+        </div>
+      </div>
+      <div class="fr-col-4">
+        2 354 543<br />Liens partagés avec DossierFacile
+      </div>
+      <div class="fr-col-8">
+        <h2>Évolution de la satisfaction des usagers</h2>
+        /semaine
+        <hr />
+        <div class="container-chart text-align-left">
+          <div id="chart3"></div>
+        </div>
+      </div>
+      <div class="fr-col-4">543<br />Partenaires utilisant DossierFacile</div>
     </div>
   </div>
 </template>
@@ -33,15 +50,17 @@ export default class Statistics extends Vue {
         "https://sheets.googleapis.com/v4/spreadsheets/1WI4vLK8eS_3N15t40NZp8SCLZBKV2St9zB68tdqMaMw/values:batchGet?key=AIzaSyAifGFaPrs6tkDizbIW8nLmtl0edfe5Vok&ranges=A1:M53&majorDimension=COLUMNS"
       )
       .then(response => {
-        console.dir(response.data);
-        console.dir(response.data["valueRanges"][0]["values"][3].splice(1, 52));
         this.updateBart(
-          response.data["valueRanges"][0]["values"][1].splice(3, 52)
+          response.data["valueRanges"][0]["values"][3]
+            .splice(1, 52)
+            .map((v: number, k: number) => {
+              return [k, v];
+            })
         );
       });
   }
 
-  updateBart(data: number[]) {
+  updateBart(data: []) {
     // set the dimensions and margins of the graph
     const margin = { top: 30, right: 30, bottom: 30, left: 60 },
       width = 460 - margin.left - margin.right,
@@ -49,7 +68,7 @@ export default class Statistics extends Vue {
 
     // append the svg object to the body of the page
     const svg = d3
-      .select("#chart2")
+      .select("#chart1")
       .append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
@@ -58,7 +77,7 @@ export default class Statistics extends Vue {
 
     const x = d3
       .scaleLinear()
-      .domain([0, 1000])
+      .domain([0, 52])
       .range([0, width]);
     svg
       .append("g")
@@ -66,30 +85,17 @@ export default class Statistics extends Vue {
       .call(d3.axisBottom(x));
 
     const y = d3
-      .scaleLog()
+      .scaleLinear()
       .range([height, 0])
-      .domain([1, 1000])
-      .base(10);
+      .domain([0, 5000]);
     svg.append("g").call(d3.axisLeft(y));
-
-    // Compute kernel density estimation
-    const kde = this.kernelDensityEstimator(
-      this.kernelEpanechnikov(7),
-      x.ticks(40)
-    );
-
-    const density = kde(
-      data.map(function(d) {
-        return d;
-      })
-    );
 
     // Plot the area
     svg
       .append("path")
       .attr("class", "mypath")
-      .datum(density)
-      .attr("fill", "#69b3a2")
+      .datum(data)
+      .attr("fill", "none")
       .attr("opacity", ".6")
       .attr("stroke", "#000")
       .attr("stroke-width", 1)
@@ -103,30 +109,10 @@ export default class Statistics extends Vue {
             return x(d[0]);
           })
           .y(function(d) {
-            console.dir(d);
-            return y(d[1] * 10000000);
+            return y(d[1]);
           })
       );
   }
-
-  // Function to compute density
-  kernelDensityEstimator(kernel: any, X: any) {
-    return function(V: any) {
-      return X.map(function(x: any) {
-        return [
-          x,
-          d3.mean(V, function(v: number) {
-            return kernel(x - v);
-          })
-        ];
-      });
-    };
-  }
-  kernelEpanechnikov = function(k: number) {
-    return function(v: number) {
-      return Math.abs((v /= k)) <= 1 ? (0.75 * (1 - v * v)) / k : 0;
-    };
-  };
 }
 </script>
 
