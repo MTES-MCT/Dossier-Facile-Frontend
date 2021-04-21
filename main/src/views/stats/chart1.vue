a39d27dc7d4280d7eda403f2dac66da24438c3b4
Matthieu Bollot <m.bollot@code-troopers.com>
2021-04-21 16:25:21 +0200

    feat(stats): add more values to stats 1

--- main/src/views/stats/chart1.vue
+++ main/src/views/stats/chart1.vue
@@ -22,17 +22,25 @@ export default class Chart1 extends Vue {
         "https://sheets.googleapis.com/v4/spreadsheets/1WI4vLK8eS_3N15t40NZp8SCLZBKV2St9zB68tdqMaMw/values:batchGet?key=AIzaSyAifGFaPrs6tkDizbIW8nLmtl0edfe5Vok&ranges=1.comptescrees_w!A1:M53&majorDimension=COLUMNS"
       )
       .then(response => {
-        this.updateChart(
-          response.data["valueRanges"][0]["values"][3]
-            .splice(1, 52)
-            .map((v: number, k: number) => {
-              return [k, v];
-            })
-        );
+        this.updateChart(response.data["valueRanges"][0]["values"]);
       });
   }
 
-  updateChart(data: []) {
+  getMaxValue(data: [number[]]) {
+    let max = 0;
+    console.dir(data);
+    for (let i = 1; i < data.length; i++) {
+      for (let ii = 1; ii < data[i].length; ii++) {
+        if (Number(data[i][ii]) > max) {
+          max = Number(data[i][ii]);
+        }
+      }
+    }
+    console.log(max);
+    return max;
+  }
+
+  updateChart(data: [any[]]) {
     // set the dimensions and margins of the graph
     const margin = { top: 30, right: 30, bottom: 30, left: 60 },
       width = 600 - margin.left - margin.right,
@@ -59,31 +67,50 @@ export default class Chart1 extends Vue {
     const y = d3
       .scaleLinear()
       .range([height, 0])
-      .domain([0, 5000]);
+      .domain([0, this.getMaxValue(data) * 1.1]);
     svg.append("g").call(d3.axisLeft(y));
 
-    // Plot the area
-    svg
-      .append("path")
-      .attr("class", "mypath")
-      .datum(data)
-      .attr("fill", "none")
-      .attr("opacity", ".6")
-      .attr("stroke", "#000")
-      .attr("stroke-width", 1)
-      .attr("stroke-linejoin", "round")
-      .attr(
-        "d",
-        d3
-          .line()
-          .curve(d3.curveBasis)
-          .x(function(d) {
-            return x(d[0]);
-          })
-          .y(function(d) {
-            return y(d[1]);
-          })
-      );
+    const colors = [
+      "#FF8D7E",
+      "#169B62",
+      "#5770BE",
+      "#A26859",
+      "#7D4E5B",
+      "#FF6F4C",
+      "#FDCF41",
+      "#D08A77",
+      "#484D7A",
+      "#958B62"
+    ];
+
+    for (let i = 1; i < data.length - 1; i++) {
+      // Plot the area
+      const localData: any = data[i]
+        .splice(1, 53)
+        .map((v: number, k: number) => {
+          return [k, v];
+        });
+      svg
+        .append("path")
+        .attr("class", "mypath")
+        .datum(localData)
+        .attr("fill", "none")
+        .attr("stroke", colors[i % 10])
+        .attr("stroke-width", 2)
+        .attr("stroke-linejoin", "round")
+        .attr(
+          "d",
+          d3
+            .line()
+            .curve(d3.curveBasis)
+            .x(function(d) {
+              return x(d[0]);
+            })
+            .y(function(d) {
+              return y(d[1]);
+            })
+        );
+    }
   }
 }
 </script>
