da2f0506e54df59f4970240dbc9f94162fa1aa65
Matthieu Bollot <m.bollot@code-troopers.com>
2021-06-14 17:02:32 +0200

    feat(oauth): fix variable name

--- df-shared/src/services/AuthService.ts
+++ df-shared/src/services/AuthService.ts
@@ -2,7 +2,7 @@ import { User } from "../models/User";
 import axios from "axios";
 
 const API_URL = `https://${process.env.VUE_APP_API_URL}/api/`;
-const SSO_ENDPOINT = process.env.VUE_APP_API_URL;
+const SSO_ENDPOINT = process.env.VUE_APP_SSO_ENDPOINT;
 const CALLBACK_URI = process.env.VUE_APP_CALLBACK_URI;
 
 export const AuthService = {
