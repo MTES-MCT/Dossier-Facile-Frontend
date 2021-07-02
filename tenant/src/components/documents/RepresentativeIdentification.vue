ae8d42c39ea748df4e0d58ca593a66324f5a8ed6
Matthieu Bollot <m.bollot@code-troopers.com>
2021-07-02 11:21:38 +0200

    feat(documents): improve file delete

--- tenant/src/components/documents/RepresentativeIdentification.vue
+++ tenant/src/components/documents/RepresentativeIdentification.vue
@@ -170,9 +170,10 @@ export default class RepresentativeIdentification extends Vue {
     if (file.path && file.id) {
       RegisterService.deleteFile(file.id);
     } else {
-      this.files = this.files.filter((f: DfFile) => {
-        return f.name !== file.name;
+      const firstIndex = this.files.findIndex(f => {
+        return f.name === file.name && f.size === file.size;
       });
+      this.files.splice(firstIndex, 1);
     }
   }
 
