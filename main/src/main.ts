3dd7c0b9b185546a69fa749beecef36d6d810302
Matthieu Bollot <m.bollot@code-troopers.com>
2021-09-06 17:55:33 +0200

    fix: 🐛 Add missing parameter to send first page

--- main/src/main.ts
+++ main/src/main.ts
@@ -58,6 +58,10 @@ if (Vue.$cookies.get("accept-cookie") === "true") {
     {
       config: {
         id: "UA-50823626-2",
+        params: {
+          // eslint-disable-next-line @typescript-eslint/camelcase
+          send_page_view: true
+        },
         linker: {
           domains: [
             "dossierfacile.fr",
