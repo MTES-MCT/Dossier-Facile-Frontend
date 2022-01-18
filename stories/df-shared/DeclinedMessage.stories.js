888956cc946af46dacad652dfc42575537516484
Matthieu Bollot <m.bollot@code-troopers.com>
2022-01-18 13:16:49 +0100

    feat: 🎸 Handle rich text on declined messages

--- stories/df-shared/DeclinedMessage.stories.js
+++ stories/df-shared/DeclinedMessage.stories.js
@@ -20,3 +20,8 @@ export const DeclinedMessage1Story = Template.bind({});
 DeclinedMessage1Story.args = {
     message: "Votre pièce d'identité n'est plus valide car elle a expiré."
 };
+
+export const DeclinedMessage2Story = Template.bind({});
+DeclinedMessage2Story.args = {
+    message: "Votre <b>pièce</b> d'identité n'est plus valide car elle a expiré."
+};
