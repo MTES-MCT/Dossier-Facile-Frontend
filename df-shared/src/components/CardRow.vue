16ea88d896e374378c82eba1c005d7c08e426167
Matthieu Bollot <m.bollot@code-troopers.com>
2022-01-18 13:55:40 +0100

    style: 💄 Imrove visibility on declined financial doc

--- df-shared/src/components/CardRow.vue
+++ df-shared/src/components/CardRow.vue
@@ -5,18 +5,30 @@
         <div class="v-center">
           <slot name="tag"></slot>
         </div>
-        <div v-if="$slots.text" class="text-container">
+        <div
+          v-if="$slots.text"
+          class="text-container"
+          :class="{ danger: danger }"
+        >
           <slot name="text"></slot>
         </div>
       </div>
       <div class="btn-container">
-        <button @click="edit()" class="fr-p-0 fr-mr-2w icon-btn">
-          <div class="color--primary material-icons md-24 fr-m-1w">
+        <button
+          @click="edit()"
+          class="fr-p-0 fr-mr-2w icon-btn"
+          :class="{ danger: danger, 'color--primary': !danger }"
+        >
+          <div class="material-icons md-24 fr-m-1w">
             edit
           </div>
         </button>
-        <button @click="remove()" class="fr-p-0 icon-btn">
-          <div class="color--primary material-icons md-24 fr-m-1w">
+        <button
+          @click="remove()"
+          class="fr-p-0 icon-btn"
+          :class="{ danger: danger, 'color--primary': !danger }"
+        >
+          <div class="material-icons md-24 fr-m-1w">
             delete
           </div>
         </button>
@@ -26,7 +38,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Prop, Vue } from "vue-property-decorator";
 import NakedCard from "./NakedCard.vue";
 
 @Component({
@@ -35,6 +47,8 @@ import NakedCard from "./NakedCard.vue";
   }
 })
 export default class CardRow extends Vue {
+  @Prop({ default: false }) danger!: boolean;
+
   edit() {
     this.$emit("edit");
   }
@@ -78,6 +92,9 @@ export default class CardRow extends Vue {
 .icon-btn {
   border: 1px solid var(--primary);
   background-color: var(--background-default-grey);
+  &.danger {
+    border: 1px solid var(--danger);
+  }
 }
 
 .text-container {
@@ -87,5 +104,9 @@ export default class CardRow extends Vue {
     flex-grow: 1;
     justify-content: center;
   }
+}
+
+.danger {
+  color: var(--danger);
 }
 </style>
