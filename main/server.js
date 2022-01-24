b92d4f424b8d13fedadb5e78de7a2f0be826d588
Matthieu Bollot <m.bollot@code-troopers.com>
2022-01-24 16:08:36 +0100

    refactor: 💡 refactor server.js

--- main/server.js
+++ main/server.js
@@ -1,25 +1,31 @@
-var express = require("express");
-var history = require('connect-history-api-fallback');
+const express = require("express");
+const history = require("connect-history-api-fallback");
 
-var app = express();
+const app = express();
 app.use(history());
 
 app.use(function(req, res, next) {
   res.setHeader(
     "Content-Security-Policy",
-    "default-src 'none'; script-src 'self' *.dossierfacile.fr https://www.google-analytics.com https://www.googletagmanager.com *.inspectlet.com ; style-src 'self' 'unsafe-inline'; img-src 'self' https://www.google-analytics.com *.helpscout.net data:; font-src 'self' fonts.google.com fonts.googleapis.com data:; object-src 'none'; prefetch-src 'self'; connect-src https://www.google-analytics.com *.inspectlet.com https://stats.g.doubleclick.net https://sheets.googleapis.com *.helpscout.net wss://ws-helpscout.pusher.com https://sockjs-helpscout.pusher.com"
+    "default-src 'none'; " +
+      "script-src 'self' *.dossierfacile.fr https://www.google-analytics.com https://www.googletagmanager.com *.inspectlet.com ; " +
+      "style-src 'self' 'unsafe-inline'; " +
+      "img-src 'self' https://www.google-analytics.com *.helpscout.net data:; " +
+      "font-src 'self' fonts.google.com fonts.googleapis.com data:; object-src 'none'; " +
+      "prefetch-src 'self'; " +
+      "connect-src https://www.google-analytics.com *.inspectlet.com https://stats.g.doubleclick.net https://sheets.googleapis.com *.helpscout.net wss://ws-helpscout.pusher.com https://sockjs-helpscout.pusher.com"
   );
   res.setHeader("X-Frame-Options", "deny");
   res.setHeader("X-Content-Type-Options", "nosniff");
   return next();
 });
 
-var directory = "/" + (process.env.STATIC_DIR || "dist");
+const directory = "/" + (process.env.STATIC_DIR || "dist");
 app.use(express.static(__dirname + directory));
 
 app.use("/robots.txt", express.static(__dirname + "robots.txt"));
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on", port);
 });
