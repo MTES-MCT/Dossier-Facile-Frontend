5847203ba028995ca58dddc899a3ec892f97aca7
Matthieu Bollot <m.bollot@code-troopers.com>
2022-01-26 11:55:31 +0100

    fix: 🐛 Use new server file for csp

--- tenant/server.js
+++ tenant/server.js
@@ -1,22 +1,34 @@
-var express = require("express");
-var history = require('connect-history-api-fallback');
+const express = require("express");
+const history = require("connect-history-api-fallback");
 
-var app = express();
+const app = express();
 app.use(history());
 
 app.use(function(req, res, next) {
   res.setHeader(
     "Content-Security-Policy",
-    "default-src 'none'; script-src 'self' *.dossierfacile.fr https://www.google-analytics.com https://www.googletagmanager.com https://www.google.com https://www.gstatic.com https://beacon-v2.helpscout.net *.inspectlet.com 'unsafe-inline' 'unsafe-eval'; style-src 'self' https://fonts.googleapis.com 'unsafe-inline'; img-src 'self' https://www.google-analytics.com https://fonts.gstatic.com https://voxusagers.numerique.gouv.fr data:; font-src 'self' fonts.google.com https://fonts.gstatic.com fonts.googleapis.com data:; object-src 'none'; prefetch-src 'self'; connect-src https://www.google-analytics.com *.inspectlet.com https://stats.g.doubleclick.net https://sso-preprod.dossierfacile.fr *.cloudfront.net *.dossierfacile.fr 'unsafe-eval' https://beacon-v2.helpscout.net; frame-src https://www.google.com; child-src *.dossierfacile.fr"
+    "default-src 'none'; " +
+      "script-src 'self' *.dossierfacile.fr https://www.google-analytics.com https://www.googletagmanager.com https://www.google.com https://www.gstatic.com https://beacon-v2.helpscout.net *.inspectlet.com 'unsafe-inline' 'unsafe-eval' blob:; " +
+      "style-src 'self' https://fonts.googleapis.com 'unsafe-inline'; " +
+      "img-src 'self' https://www.google-analytics.com https://fonts.gstatic.com https://voxusagers.numerique.gouv.fr https://hn.inspectlet.com *.helpscout.net data:; " +
+      "font-src 'self' fonts.google.com https://fonts.gstatic.com fonts.googleapis.com data:; " +
+      "object-src 'none'; " +
+      "prefetch-src 'self'; " +
+      "connect-src https://www.google-analytics.com *.inspectlet.com wss://ws.inspectlet.com https://stats.g.doubleclick.net https://sso-preprod.dossierfacile.fr *.cloudfront.net *.dossierfacile.fr 'unsafe-eval' *.helpscout.net wss://ws-helpscout.pusher.com https://sockjs-helpscout.pusher.com blob:; " +
+      "frame-src https://www.google.com *.dossierfacile.fr; " +
+      "child-src *.dossierfacile.fr"
   );
   res.setHeader("X-Frame-Options", "deny");
+  res.setHeader("X-Content-Type-Options", "nosniff");
   return next();
 });
 
-var directory = "/" + (process.env.STATIC_DIR || "dist");
+const directory = "/" + (process.env.STATIC_DIR || "dist");
 app.use(express.static(__dirname + directory));
 
-var port = process.env.PORT || 3000;
+app.use("/robots.txt", express.static(__dirname + "robots.txt"));
+
+const port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on", port);
 });
