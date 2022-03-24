50c1a53ef62669b11e40a62e684e3b98c5bd2812
Matthieu Bollot <m.bollot@code-troopers.com>
2022-03-24 11:12:32 +0100

    feat(owner): 🎸 Add delete property button

--- owner/src/components/property/ConsultProperty.vue
+++ owner/src/components/property/ConsultProperty.vue
@@ -1,36 +1,39 @@
 <template>
   <div class="fr-container fr-container--fluid fluid-full-width position--relative">
-  <div class="bg-pic position--absolute">
-  </div>
-  <div class="fr-container position--relative mt-100">
-  <div class="fr-grid-row space-between fr-mb-3w">
-    <div class="fr-grid-row">
-      <router-link class="fr-btn btn--white fr-btn--secondary" to="/">{{ t('back') }}</router-link>
-      <div class="title">{{ name }}</div>
+    <div class="bg-pic position--absolute"></div>
+    <div class="fr-container position--relative mt-100">
+      <div class="fr-grid-row space-between fr-mb-3w">
+        <div class="fr-grid-row">
+          <router-link class="fr-btn btn--white fr-btn--secondary" to="/">{{
+            t("back")
+          }}</router-link>
+          <div class="title">{{ name }}</div>
+        </div>
+        <div class="fr-grid-row">
+          <button class="fr-btn btn--white fr-btn--secondary">
+            <span class="material-icons md-24"> share_variant </span>
+          </button>
+          <button @click="editProperty()" class="fr-btn btn--white fr-btn--secondary fr-ml-1w">
+            {{ t("modify-property") }}
+          </button>
+          <button class="fr-btn btn--white fr-btn--secondary fr-ml-1w">
+            {{ t("delete-property") }}
+          </button>
+        </div>
+      </div>
+      <NakedCard class="h-100">
+        <div>{{ token }}</div>
+      </NakedCard>
+      <NakedCard class="fr-mt-3w">
+        {{ p }}
+      </NakedCard>
     </div>
-    <div class="fr-grid-row">
-      <button class="fr-btn btn--white fr-btn--secondary">
-          <span class="material-icons md-24">
-            share_variant
-          </span>
-      </button>
-      <button class="fr-btn btn--white fr-btn--secondary fr-ml-1w">modify</button>
-      <button class="fr-btn btn--white fr-btn--secondary fr-ml-1w">delete</button>
-    </div>
-  </div>
-    <NakedCard class="h-100">
-    <div>{{ token }}</div>
-    </NakedCard>
-    <NakedCard class="fr-mt-3w">
-    {{p}}
-    </NakedCard>
-  </div>
   </div>
 </template>
 <script setup lang="ts">
 import { computed, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 import { useStore } from 'vuex';
 import NakedCard from 'df-shared/src/components/NakedCard.vue';
 
@@ -39,6 +42,7 @@ const { t } = useI18n();
 defineProps<{}>();
 
 const route = useRoute();
+const router = useRouter();
 const store = useStore();
 
 const id = ref(0);
@@ -50,6 +54,10 @@ if (route.params.id) {
 const token = computed(() => store.getters.getPropertyToConsult?.token);
 const name = computed(() => store.getters.getPropertyToConsult?.name);
 const p = store.getters.getPropertyToConsult;
+
+function editProperty() {
+  router.push({ name: 'PropertyName', params: { id: id.value } });
+}
 </script>
 
 <style scoped lang="scss">
@@ -61,7 +69,7 @@ const p = store.getters.getPropertyToConsult;
   background-size: cover !important;
   background-position: 50% 40% !important;
   background-image: linear-gradient(0deg, rgba(22, 22, 22, 0.7), rgba(22, 22, 22, 0.7)),
-   url("../../assets/salon.webp");
+    url("../../assets/salon.webp");
   background-color: #314467;
   z-index: 0;
 }
@@ -97,11 +105,15 @@ const p = store.getters.getPropertyToConsult;
 {
   "en": {
     "title": "consult",
-    "back": "Back"
+    "back": "Back",
+    "modify-property": "Modify my property",
+    "delete-property": "Delete my property"
   },
   "fr": {
     "title": "Consultation",
-    "back": "Retour"
+    "back": "Retour",
+    "modify-property": "Modifier ma propriété",
+    "delete-property": "Supprimer ma propriété"
   }
 }
 </i18n>
