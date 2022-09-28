a72d0c68277ccf9df76a3b6dc9b33b455339e8b8
Juliette de Rancourt <juliette@dossierfacile.fr>
2022-09-28 10:35:04 +0200

    feat: Allow user to dismiss the announcement (#789)

--- df-shared-next/src/components/Announcement.vue
+++ df-shared-next/src/components/Announcement.vue
@@ -1,15 +1,45 @@
 <script setup lang="ts">
+import { useCookies } from "vue3-cookies";
+const { cookies } = useCookies();
+
 const MESSAGE = `${import.meta.env.VITE_ANNOUNCEMENT_MESSAGE}`;
+const MAIN_URL = `//${import.meta.env.VUE_APP_MAIN_URL}`;
+
+const announcementClosedCookieKey = `announcement-closed-${btoa(MESSAGE)}`;
+let announcementClosed = cookies.get(announcementClosedCookieKey) === "true";
 
 function isVisible() {
-  return MESSAGE.trim().length > 0;
+  const isMessageSet = MESSAGE.trim().length > 0;
+  return isMessageSet && !announcementClosed;
+}
+
+function createAnnouncementClosedCookie() {
+  const expirationDate = new Date();
+  expirationDate.setDate(expirationDate.getDate() + 1);
+  const domain = MAIN_URL.endsWith("dossierfacile.fr")
+    ? "dossierfacile.fr"
+    : "";
+
+  cookies.set(
+    announcementClosedCookieKey,
+    "true",
+    expirationDate.toUTCString(),
+    "",
+    domain
+  );
+}
+
+function closeAnnouncement() {
+  createAnnouncementClosedCookie();
+  announcementClosed = true;
 }
 </script>
 
 <template>
   <div class="announcement fr-pt-2w fr-pb-2w" v-if="isVisible()">
-    <div class="fr-container">
+    <div class="fr-container message">
       {{ MESSAGE }}
+      <button class="fr-btn--close close" @click="closeAnnouncement()"></button>
     </div>
   </div>
 </template>
@@ -17,6 +47,13 @@ function isVisible() {
 <style scoped lang="scss">
 .announcement {
   background-color: #45a07a;
+  color: #fff;
+}
+.message {
+  display: flex;
+  align-items: center;
+}
+.close {
   color: #fff;
 }
 </style>
--- df-shared/src/components/Announcement.vue
+++ df-shared/src/components/Announcement.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="announcement fr-pt-2w fr-pb-2w" v-if="isVisible()">
-    <div class="fr-container">
+    <div class="fr-container message">
       {{ MESSAGE }}
+      <button class="fr-btn--close close" @click="closeAnnouncement()"></button>
     </div>
   </div>
 </template>
@@ -12,9 +13,36 @@ import { Vue, Component } from "vue-property-decorator";
 @Component
 export default class Announcement extends Vue {
   MESSAGE = `${process.env.VUE_APP_ANNOUNCEMENT_MESSAGE}`;
+  MAIN_URL = `//${process.env.VUE_APP_MAIN_URL}`;
+
+  announcementClosedCookieKey = `announcement-closed-${btoa(this.MESSAGE)}`;
+  announcementClosed =
+    this.$cookies.get(this.announcementClosedCookieKey) === "true";
 
   isVisible() {
-    return this.MESSAGE.trim().length > 0;
+    const isMessageSet = this.MESSAGE.trim().length > 0;
+    return isMessageSet && !this.announcementClosed;
+  }
+
+  closeAnnouncement() {
+    this.createAnnouncementClosedCookie();
+    this.announcementClosed = true;
+  }
+
+  createAnnouncementClosedCookie() {
+    const expirationDate = new Date();
+    expirationDate.setDate(expirationDate.getDate() + 1);
+    const domain = this.MAIN_URL.endsWith("dossierfacile.fr")
+      ? "dossierfacile.fr"
+      : "localhost";
+
+    this.$cookies.set(
+      this.announcementClosedCookieKey,
+      true,
+      expirationDate.toUTCString(),
+      "",
+      domain
+    );
   }
 }
 </script>
@@ -22,6 +50,13 @@ export default class Announcement extends Vue {
 <style scoped lang="scss">
 .announcement {
   background-color: #45a07a;
+  color: #fff;
+}
+.message {
+  display: flex;
+  align-items: center;
+}
+.close {
   color: #fff;
 }
 </style>
