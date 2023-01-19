802af4f163a07f98660074cc85caf998c9bf32e4
Juliette <juliette@dossierfacile.fr>
2023-01-19 16:00:28 +0100

    ci: Run e2e tests

--- main/cypress.config.ts
+++ main/cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:9001"
+    baseUrl: "http://localhost:9001",
+    video: false
   }
 });
