b0ace6c51d2d54f68bd5b5473924e0cf80f40720
Juliette <juliette@dossierfacile.fr>
2023-01-20 17:21:55 +0100

    test(main): Add e2e accessibility tests

---
+++ main/cypress/e2e/accessibility.cy.ts
@@ -0,0 +1,23 @@
+const routesToTest = [
+  "/",
+  "/information",
+  "/blog",
+  "/contact",
+  "/stats",
+  "/plan-du-site",
+  "/accessibilite"
+];
+
+beforeEach(() => {
+  cy.mockStats();
+});
+
+describe("accessibility tests", () => {
+  routesToTest.forEach(route => {
+    it(`${route} has no detectable accessibility violations on load`, () => {
+      cy.visit(route);
+      cy.injectAxe();
+      cy.checkA11y();
+    });
+  });
+});
