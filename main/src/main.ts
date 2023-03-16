f74ef40327e60484864f17dd554e0a27ebf41a15
Matthieu Bollot <m.bollot@code-troopers.com>
2023-03-16 16:33:12 +0100

    chore: remove old iphub scripts

--- main/src/main.ts
+++ main/src/main.ts
@@ -40,17 +40,7 @@ const inspectlet = function() {
   setTimeout(ldinsp, 0);
 };
 
-const iphub = function() {
-  JQuery(document).ready(function() {
-    JQuery(document.body).prepend(
-      "<script src='/iphubb.js'></script>" +
-        '<noscript> <iframe src="https://3689183.fls.doubleclick.net/activityi;src=3689183;type=dossi0;cat=landingp;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1;num=1?" width="1" height="1" frameborder="0" style="display:none"></iframe> </noscript>'
-    );
-  });
-};
-
 Vue.prototype.inspectlet = inspectlet;
-Vue.prototype.iphub = iphub;
 
 Vue.config.productionTip = false;
 
