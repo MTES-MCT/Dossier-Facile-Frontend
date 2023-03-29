edc94ad2413207afcf4077283689b4fc39a7ac68
Matthieu Bollot <m.bollot@code-troopers.com>
2023-03-29 16:15:26 +0200

    chore: Fix build

--- watermark/vite.config.ts
+++ watermark/vite.config.ts
@@ -1,10 +1,16 @@
-import { vueI18n } from '@intlify/vite-plugin-vue-i18n';
 import { defineConfig } from 'vite'
-import path from 'path'
 import vue from '@vitejs/plugin-vue'
+import vueI18n from '@intlify/vite-plugin-vue-i18n'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 9184,
+    fs: {
+      allow: ['..']
+    }
+  },
   plugins: [vue(),
-    vueI18n({})]
+  vueI18n({})
+]
 })
