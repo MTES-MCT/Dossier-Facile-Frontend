aaba11bd9094dd6555e172915491a95bf7ee7b74
Juliette <juliette@dossierfacile.fr>
2023-04-27 11:34:29 +0200

    feat: Use DSFR component for social network links in footer

--- df-shared/src/Footer/Footer.vue
+++ df-shared/src/Footer/Footer.vue
@@ -16,7 +16,7 @@
             />
           </a>
         </div>
-        <div class="fr-footer__content ">
+        <div class="fr-footer__content">
           <p class="fr-footer__content-desc">
             DossierFacile est propulsé par la
             <a
@@ -137,67 +137,6 @@
               {{ $t("sources") }}
             </a>
           </li>
-
-          <li class="fr-footer__bottom-item">
-            <ul class="fr-follow">
-              <li>
-                <a
-                  class="fr-btn--twitter fr-btn"
-                  title="Suivre Dossier facile sur twitter - Ouvre une nouvelle fenêtre"
-                  href="https://twitter.com/dossierfacile/"
-                  target="_blank"
-                  ><span class="sr-only"
-                    >Suivre Dossier facile sur twitter</span
-                  ></a
-                >
-              </li>
-              <li>
-                <a
-                  class="fr-btn--facebook fr-btn"
-                  title="Suivre Dossier facile sur facebook - Ouvre une nouvelle fenêtre"
-                  href="https://www.facebook.com/DossierFacile.fr/"
-                  target="_blank"
-                  ><span class="sr-only"
-                    >Suivre Dossier facile sur facebook</span
-                  ></a
-                >
-              </li>
-              <li>
-                <a
-                  class="fr-btn--linkedin fr-btn"
-                  lang="en"
-                  title="Suivre Dossier facile sur linkedin - Ouvre une nouvelle fenêtre"
-                  href="https://www.linkedin.com/company/startup-d-etat-locatio/"
-                  target="_blank"
-                  ><span class="sr-only"
-                    >Suivre Dossier facile sur linkedin</span
-                  ></a
-                >
-              </li>
-              <li>
-                <a
-                  class="fr-btn--instagram fr-btn"
-                  title="Suivre Dossier facile sur instagram - Ouvre une nouvelle fenêtre"
-                  href="https://www.instagram.com/dossierfacile/"
-                  target="_blank"
-                  ><span class="sr-only"
-                    >Suivre Dossier facile sur instagram</span
-                  ></a
-                >
-              </li>
-              <li>
-                <a
-                  class="fr-btn--tiktok fr-btn"
-                  title="Suivre Dossier facile sur tiktok - Ouvre une nouvelle fenêtre"
-                  href="https://www.tiktok.com/@dossierfacile/"
-                  target="_blank"
-                  ><span class="sr-only"
-                    >Suivre Dossier facile sur tiktok</span
-                  ></a
-                >
-              </li>
-            </ul>
-          </li>
         </ul>
         <div class="fr-footer__bottom-copy">
           <p>
@@ -208,6 +147,70 @@
               >licence etalab-2.0</a
             >
           </p>
+        </div>
+      </div>
+    </div>
+    <div class="fr-follow">
+      <div class="fr-container">
+        <div class="fr-grid-row">
+          <div class="fr-col-12">
+            <div class="fr-follow__social">
+              <h2 class="fr-h5">Suivez-nous sur les réseaux sociaux</h2>
+              <ul class="fr-btns-group">
+                <li>
+                  <a
+                    class="fr-btn--twitter fr-btn"
+                    title="Suivre Dossier facile sur twitter - Ouvre une nouvelle fenêtre"
+                    href="https://twitter.com/dossierfacile/"
+                    target="_blank"
+                  >
+                    Suivre Dossier facile sur twitter
+                  </a>
+                </li>
+                <li>
+                  <a
+                    class="fr-btn--facebook fr-btn"
+                    title="Suivre Dossier facile sur facebook - Ouvre une nouvelle fenêtre"
+                    href="https://www.facebook.com/DossierFacile.fr/"
+                    target="_blank"
+                  >
+                    Suivre Dossier facile sur facebook
+                  </a>
+                </li>
+                <li>
+                  <a
+                    class="fr-btn--linkedin fr-btn"
+                    lang="en"
+                    title="Suivre Dossier facile sur linkedin - Ouvre une nouvelle fenêtre"
+                    href="https://www.linkedin.com/company/startup-d-etat-locatio/"
+                    target="_blank"
+                  >
+                    Suivre Dossier facile sur linkedin
+                  </a>
+                </li>
+                <li>
+                  <a
+                    class="fr-btn--instagram fr-btn"
+                    title="Suivre Dossier facile sur instagram - Ouvre une nouvelle fenêtre"
+                    href="https://www.instagram.com/dossierfacile/"
+                    target="_blank"
+                  >
+                    Suivre Dossier facile sur instagram
+                  </a>
+                </li>
+                <li>
+                  <a
+                    class="fr-btn--tiktok fr-btn"
+                    title="Suivre Dossier facile sur tiktok - Ouvre une nouvelle fenêtre"
+                    href="https://www.tiktok.com/@dossierfacile/"
+                    target="_blank"
+                  >
+                    Suivre Dossier facile sur tiktok
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
