334b1ac87b9117d57c9aa0ff2c15237a3cee25fa
Juliette <juliette@dossierfacile.fr>
2023-05-11 12:08:27 +0200

    refactor(tenant): Introduce TextField and use it in identity forms

--- tenant/src/components/NameInformationForm.vue
+++ tenant/src/components/NameInformationForm.vue
@@ -47,144 +47,57 @@
         >
           <div class="fr-grid-row fr-grid-row--center">
             <div class="fr-col-12 fr-mb-3w">
-              <validation-provider
-                rules="required|only-alpha"
-                v-slot="{ errors, valid }"
+              <TextField
+                name="lastname"
+                :fieldLabel="$tc('nameinformationform.lastname')"
+                validation-rules="only-alpha"
+                v-model="lastname"
+                :required="true"
+                :disabled="user.franceConnect"
+              />
+              <button
+                class="fr-btn fr-btn--sm fr-btn--tertiary fr-btn--icon-left fr-icon-add-line fr-mt-1w"
+                v-if="!displayPreferredNameField"
+                @click="displayPreferredNameField = true"
               >
-                <div
-                  class="fr-input-group"
-                  :class="errors[0] ? 'fr-input-group--error' : ''"
-                >
-                  <FieldLabel :required="true" for-input="lastname">
-                    {{ $t("nameinformationform.lastname") }}
-                  </FieldLabel>
-                  <input
-                    v-model="lastname"
-                    class="form-control fr-input validate-required"
-                    :class="{
-                      'fr-input--valid': valid,
-                      'fr-input--error': errors[0]
-                    }"
-                    id="lastname"
-                    name="lastname"
-                    autocomplete="family-name"
-                    :disabled="user.franceConnect"
-                    type="text"
-                    required
-                  />
-                  <span class="fr-error-text" v-if="errors[0]">{{
-                    $t(errors[0])
-                  }}</span>
-                  <button
-                    class="fr-btn fr-btn--sm fr-btn--tertiary fr-btn--icon-left fr-icon-add-line fr-mt-1w"
-                    v-if="!displayPreferredNameField"
-                    @click="displayPreferredNameField = true"
-                  >
-                    {{ $t("nameinformationform.add-preferredname") }}
-                  </button>
-                </div>
-              </validation-provider>
+                {{ $t("nameinformationform.add-preferredname") }}
+              </button>
             </div>
             <div class="fr-col-12 fr-mb-3w" v-if="displayPreferredNameField">
-              <validation-provider
-                rules="only-alpha"
-                v-slot="{ errors, valid }"
+              <TextField
+                name="preferredname"
+                :fieldLabel="$tc('nameinformationform.preferredname')"
+                validation-rules="only-alpha"
+                v-model="preferredname"
               >
-                <div
-                  class="fr-input-group"
-                  :class="errors[0] ? 'fr-input-group--error' : ''"
-                >
-                  <FieldLabel for-input="preferredname">
-                    {{ $t("nameinformationform.preferredname") }}
-                  </FieldLabel>
-                  <div class="field-with-button fr-input-wrap">
-                    <input
-                      v-model="preferredname"
-                      class="form-control fr-input validate-required"
-                      :class="{
-                        'fr-input--valid': valid,
-                        'fr-input--error': errors[0],
-                      }"
-                      id="preferredname"
-                      name="preferredname"
-                      autocomplete="off"
-                      type="text"
-                    />
-                    <button
-                      class="fr-btn fr-btn--tertiary fr-icon-close-line fr-ml-1w"
-                      :title="$t('nameinformationform.delete-preferredname')"
-                      @click="deletePreferredName()"
-                    >
-                      {{ $t("nameinformationform.delete-preferredname") }}
-                    </button>
-                  </div>
-                  <span class="fr-error-text" v-if="errors[0]">{{
-                    $t(errors[0])
-                  }}</span>
-                </div>
-              </validation-provider>
+                <template v-slot:right>
+                  <button
+                    class="fr-btn fr-btn--tertiary fr-icon-close-line"
+                    :title="$t('nameinformationform.delete-preferredname')"
+                    @click="deletePreferredName()"
+                  >
+                    {{ $t("nameinformationform.delete-preferredname") }}
+                  </button>
+                </template>
+              </TextField>
             </div>
             <div class="fr-col-12 fr-mb-3w">
-              <validation-provider
-                rules="required|only-alpha"
-                v-slot="{ errors, valid }"
-              >
-                <div
-                  class="fr-input-group"
-                  :class="errors[0] ? 'fr-input-group--error' : ''"
-                >
-                  <FieldLabel :required="true" for-input="firstname">
-                    {{ $t("nameinformationform.firstname") }}
-                  </FieldLabel>
-                  <input
-                    id="firstname"
-                    type="text"
-                    v-model="firstname"
-                    name="firstname"
-                    autocomplete="given-name"
-                    class="validate-required form-control fr-input"
-                    :class="{
-                      'fr-input--valid': valid,
-                      'fr-input--error': errors[0]
-                    }"
-                    :disabled="user.franceConnect"
-                    required
-                  />
-                  <span class="fr-error-text" v-if="errors[0]">{{
-                    $t(errors[0])
-                  }}</span>
-                </div>
-              </validation-provider>
+              <TextField
+                name="firstname"
+                :fieldLabel="$tc('nameinformationform.firstname')"
+                validation-rules="only-alpha"
+                v-model="firstname"
+                :required="true"
+                :disabled="user.franceConnect"
+              />
             </div>
             <div class="fr-col-12 fr-mb-3w">
-              <validation-provider
-                :rules="{ zipcode: /^[0-9]{5}$/ }"
-                v-slot="{ errors, valid }"
-              >
-                <div
-                  class="fr-input-group"
-                  :class="errors[0] ? 'fr-input-group--error' : ''"
-                >
-                  <FieldLabel for-input="zipcode">
-                    {{ $t("nameinformationform.zipcode") }}
-                  </FieldLabel>
-                  <input
-                    id="zipcode"
-                    :class="{
-                      'fr-input--valid': valid,
-                      'fr-input--error': errors[0]
-                    }"
-                    type="text"
-                    v-model="zipcode"
-                    name="zipcode"
-                    autocomplete="postal-code"
-                    class="validate-required form-control fr-input"
-                  />
-                  <span class="fr-error-text" v-if="errors[0]">{{
-                    $t(errors[0])
-                  }}</span>
-                </div>
-              </validation-provider>
+              <TextField
+                name="zipcode"
+                :fieldLabel="$tc('nameinformationform.zipcode')"
+                validation-rules="zipcode"
+                v-model="zipcode"
+              />
             </div>
           </div>
           <ProfileFooter :showBack="false"></ProfileFooter>
@@ -198,10 +111,8 @@
 import { Component, Vue } from "vue-property-decorator";
 import { User } from "df-shared/src/models/User";
 import { extend, ValidationObserver, ValidationProvider } from "vee-validate";
-import { regex } from "vee-validate/dist/rules";
 import SubmitButton from "df-shared/src/Button/SubmitButton.vue";
 import RequiredFieldsInstruction from "df-shared/src/components/form/RequiredFieldsInstruction.vue";
-import FieldLabel from "df-shared/src/components/form/FieldLabel.vue";
 import NameInformationHelp from "./helps/NameInformationHelp.vue";
 import ConfirmModal from "df-shared/src/components/ConfirmModal.vue";
 import { AnalyticsService } from "../services/AnalyticsService";
@@ -209,10 +120,11 @@ import ProfileFooter from "./footer/ProfileFooter.vue";
 import { mapGetters } from "vuex";
 import NakedCard from "df-shared/src/components/NakedCard.vue";
 import { UtilsService } from "@/services/UtilsService";
+import TextField from "df-shared/src/components/form/TextField.vue";
 
 extend("zipcode", {
-  ...regex,
-  message: "nameinformationform.zipcode-not-valid"
+  validate: (field) => new RegExp(/^[0-9]{5}$/).test(field),
+  message: "nameinformationform.zipcode-not-valid",
 });
 
 @Component({
@@ -225,13 +137,13 @@ extend("zipcode", {
     ProfileFooter,
     NakedCard,
     RequiredFieldsInstruction,
-    FieldLabel,
+    TextField,
   },
   computed: {
     ...mapGetters({
-      user: "userToEdit"
-    })
-  }
+      user: "userToEdit",
+    }),
+  },
 })
 export default class NameInformationForm extends Vue {
   public user!: User;
@@ -268,7 +180,7 @@ export default class NameInformationForm extends Vue {
             this.$router.push("/reset-password/null");
           }
         },
-        error => {
+        (error) => {
           console.dir(error);
         }
       )
@@ -299,7 +211,7 @@ export default class NameInformationForm extends Vue {
           AnalyticsService.confirmName();
           this.$router.push({ name: "TenantType" });
         },
-        error => {
+        (error) => {
           console.dir(error);
         }
       )
