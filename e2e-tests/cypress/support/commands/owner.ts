6bf5764bf60ea4025b175bbbfc942d90792af3c8
Juliette <juliette@dossierfacile.fr>
2023-05-24 10:08:22 +0200

    fix(e2e): Fix tests broken by recent evolutions

--- e2e-tests/cypress/support/commands/owner.ts
+++ e2e-tests/cypress/support/commands/owner.ts
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 Cypress.Commands.add("ownerLogin", (username: string) => {
-  cy.visit("www-dev.dossierfacile.fr");
-  cy.contains("Espace propriétaire").click();
+  cy.visit("proprietaire-dev.dossierfacile.fr");
+  cy.contains("Se connecter").click();
   cy.loginWithFC(username);
 });
