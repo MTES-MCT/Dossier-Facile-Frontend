a7937975e44c37257edaf6b0ee75b45ee196e704
Juliette de Rancourt <juliette@dossierfacile.fr>
2023-08-09 15:14:35 +0200

    refactor(watermark): Extract main page into components

---
+++ watermark/src/components/WatermarkingPage.vue
@@ -0,0 +1,138 @@
+<script setup lang="ts">
+import Downloader from "./Downloader.vue";
+import ProgressIndicator from "./ProgressIndicator.vue";
+import axios from "axios";
+import { ref } from "vue";
+import { useI18n } from "vue-i18n";
+import Feedback from "./Feedback.vue";
+
+const files = ref<File[]>([]);
+const token = ref("");
+const wait = ref(false);
+const url = ref("");
+const watermark = ref("");
+
+const { t } = useI18n();
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+async function handleSubmit() {
+  if (files.value === null) {
+    return;
+  }
+  wait.value = true;
+  url.value = "";
+  const formData = new FormData();
+  files.value.forEach((file) => formData.append(`files[]`, file));
+  formData.append(`watermark`, watermark.value);
+
+  axios
+    .post(`${API_URL}/api/document/files`, formData)
+    .then((res: any) => {
+      token.value = res.data.token;
+      getFile();
+    })
+    .catch((err: any) => {
+      console.dir(err);
+      wait.value = false;
+    });
+}
+
+function handleChange(e: any) {
+  url.value = "";
+  files.value = Array.from(e.target.files);
+}
+
+function getWatermarkedFileName() {
+  const originalFileNames = files.value.map((file) => file.name.split(".")[0]);
+  return originalFileNames.join("_") + "_filigrane";
+}
+
+function getFile() {
+  axios
+    .get(`${API_URL}/api/document/url/${token.value}`)
+    .then((res: any) => {
+      if (res.data.url) {
+        url.value = `${API_URL}/api/document/${token.value}`;
+        wait.value = false;
+      }
+    })
+    .catch(() => {
+      setTimeout(function () {
+        getFile();
+      }, 5000);
+    });
+}
+</script>
+
+<template>
+  <h1 class="fr-h4 fr-mt-3w">
+    {{ t("watermarking-page.title") }}
+  </h1>
+  <div class="fr-alert fr-alert--info fr-mb-3w">
+    <p v-html="t('watermarking-page.behaviour-1')"></p>
+    <p v-html="t('watermarking-page.behaviour-2')"></p>
+    <p v-html="t('watermarking-page.behaviour-3')"></p>
+  </div>
+  <form name="uploadForm" @submit.prevent="handleSubmit">
+    <div class="fr-grid-row fr-grid-row--center">
+      <div class="fr-col-12 fr-mb-3w">
+        <div class="fr-input-group">
+          <div class="fr-upload-group">
+            <label class="fr-label" for="file-upload"
+              >{{ t("watermarking-page.add-file-label") }}
+              <span class="fr-hint-text"
+                >Formats supportés : jpg, png, pdf.</span
+              >
+            </label>
+            <input
+              class="fr-upload"
+              type="file"
+              name="files"
+              @change="handleChange"
+              accept="image/png, image/jpeg, application/pdf"
+              multiple
+            />
+          </div>
+        </div>
+      </div>
+      <div class="fr-col-12 fr-mb-3w">
+        <div class="fr-input-group">
+          <div class="fr-text-group">
+            <label class="fr-label" for="text">
+              {{ t("watermarking-page.add-watermark-label") }}
+            </label>
+            <input
+              class="fr-input"
+              type="text"
+              v-model="watermark"
+              :placeholder="'Ex : Document exclusivement destiné à la location immobilière'"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div class="fr-col-12 text-center fr-mb-5w">
+        <button class="fr-btn" type="submit" :disabled="wait && !url">
+          <span v-if="!wait || url">
+            {{ t("watermarking-page.submit") }}
+          </span>
+          <span v-if="wait && !url">
+            {{ t("watermarking-page.wait") }}
+            <ProgressIndicator diameter="22px" border="3px" color="#000091" />
+          </span>
+        </button>
+      </div>
+
+      <Downloader
+        v-if="url"
+        :url="url"
+        :fileName="getWatermarkedFileName()"
+        @on-downloaded="url = ''"
+      />
+    </div>
+  </form>
+  <Feedback />
+</template>
+
+<style scoped lang="scss"></style>
