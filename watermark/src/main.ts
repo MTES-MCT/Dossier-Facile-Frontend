831f9c8a843b7249942dc6ee12f06ad8930487f6
Juliette de Rancourt <juliette@dossierfacile.fr>
2023-08-09 17:46:03 +0200

    chore(watermark): Send errors to sentry

--- watermark/src/main.ts
+++ watermark/src/main.ts
@@ -1,13 +1,36 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import i18n from './i18n';
-import Toast from 'vue-toastification';
-import 'vue-toastification/dist/index.css';
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import i18n from "./i18n";
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+import * as Sentry from "@sentry/vue";
+import { BrowserTracing } from "@sentry/tracing";
 import router from "./router";
 
-const app = createApp(App)
+const ENVIRONMENT = import.meta.env.VITE_ENVIRONMENT;
+
+const app = createApp(App);
+
+Sentry.init({
+  app,
+  dsn: "https://1e0bc34f71b74ed3b70019949edf107c@sentry.incubateur.net/113",
+  environment: ENVIRONMENT,
+  integrations: [
+    new BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracingOrigins: [
+        "localhost",
+        "filigrane-dev.dossierfacile.fr",
+        "filigrane.beta.gouv.fr",
+        /^\//,
+      ],
+    }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
 app.use(i18n);
 app.use(Toast as any);
 app.use(router);
-app.mount('#app');
+app.mount("#app");
