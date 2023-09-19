760c706954d308d4e6ff0771fa03723e65f8c4a2
Matthieu Bollot <m.bollot@code-troopers.com>
2023-09-19 16:52:08 +0200

    chore: clean analytics and doubleclick remaining

--- main/server.js
+++ main/server.js
@@ -7,12 +7,12 @@ app.use(function(req, res, next) {
   res.setHeader(
     "Content-Security-Policy",
     "default-src 'none'; " +
-      "script-src 'self' *.dossierfacile.fr https://www.google-analytics.com https://stats.data.gouv.fr https://www.googletagmanager.com https://beacon-v2.helpscout.net 'unsafe-eval' 'unsafe-inline' ; " +
+      "script-src 'self' *.dossierfacile.fr https://stats.data.gouv.fr https://www.googletagmanager.com https://beacon-v2.helpscout.net 'unsafe-eval' 'unsafe-inline' ; " +
       "style-src 'self' https://cdn.jsdelivr.net/npm/remixicon@3.2.0/fonts/remixicon.min.css 'unsafe-inline' fonts.googleapis.com ; " +
-      "img-src 'self' https://www.google-analytics.com *.helpscout.net *.cloudfront.net *.gitbook.com data:; " +
+      "img-src 'self' *.helpscout.net *.cloudfront.net *.gitbook.com data:; " +
       "font-src 'self' https://cdn.jsdelivr.net/npm/remixicon@3.2.0/fonts/ fonts.gstatic.com fonts.google.com fonts.googleapis.com data:; object-src 'none'; " +
-      "frame-src *.fls.doubleclick.net/; " +
-      "connect-src https://sentry.incubateur.net https://www.google-analytics.com  https://stats.data.gouv.fr https://stats.g.doubleclick.net https://sheets.googleapis.com *.helpscout.net wss://ws-helpscout.pusher.com https://sockjs-helpscout.pusher.com *.cloudfront.net *.dossierfacile.fr"
+      "frame-src ; " +
+      "connect-src https://sentry.incubateur.net https://stats.data.gouv.fr https://sheets.googleapis.com *.helpscout.net wss://ws-helpscout.pusher.com https://sockjs-helpscout.pusher.com *.cloudfront.net *.dossierfacile.fr"
   );
   res.setHeader("X-Frame-Options", "deny");
   res.setHeader("X-Content-Type-Options", "nosniff");
