8a329e130ccba9c834d689135a78166aa35b9397
Juliette de Rancourt <juliette@dossierfacile.fr>
2023-11-09 18:47:47 +0100

    feat: Store acquisition data in cookie and send it at first login

--- df-shared/src/services/CookiesService.ts
+++ df-shared/src/services/CookiesService.ts
@@ -12,8 +12,23 @@ export const CookiesService = {
   getCookie(name: string) {
     return Vue.$cookies.get(name);
   },
+  setJsonCookie(name: string, value: any, expires?: string | Date) {
+    const json = JSON.stringify(value);
+    const base64 = btoa(json);
+    Vue.$cookies.set(name, base64, expires, "", DOMAIN);
+  },
+  getJsonCookie(name: string) {
+    const base64 = Vue.$cookies.get(name);
+    if (base64 != undefined) {
+      const json = atob(base64);
+      return JSON.parse(json);
+    }
+  },
   isTrue(cookieName: string) {
     return this.getCookie(cookieName) === "true";
+  },
+  deleteCookie(name: string) {
+    Vue.$cookies.remove(name);
   },
   datePlusDaysFromNow(days: number) {
     const date = new Date();
