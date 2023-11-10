d81e2e99e74788e38126f6777fefdf3f06908634
Juliette de Rancourt <juliette@dossierfacile.fr>
2023-11-10 16:41:41 +0100

    feat(owner): Store acquisition data in cookie and send it at first login

---
+++ df-shared-next/src/services/CookiesService.ts
@@ -0,0 +1,28 @@
+import { useCookies } from "vue3-cookies";
+
+const DOMAIN = `${import.meta.env.VITE_DOMAIN}`;
+
+const { cookies } = useCookies();
+
+export const CookiesService = {
+  setJsonCookie(name: string, value: any, expires?: string | Date) {
+    const json = JSON.stringify(value);
+    const base64 = btoa(json);
+    cookies.set(name, base64, expires, "/", DOMAIN);
+  },
+  getJsonCookie(name: string) {
+    const base64 = cookies.get(name);
+    if (base64 != undefined) {
+      const json = atob(base64);
+      return JSON.parse(json);
+    }
+  },
+  deleteCookie(name: string) {
+    cookies.remove(name);
+  },
+  datePlusDaysFromNow(days: number) {
+    const date = new Date();
+    date.setDate(date.getDate() + days);
+    return date;
+  },
+};
