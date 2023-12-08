2a7a7fa7a8a34cb64937b31b361ed90c1b4b1835
Matthieu Bollot <m.bollot@code-troopers.com>
2023-12-08 16:30:42 +0100

    hotfix: totally remove sentry on watermark (#1303)

--- watermark/src/main.ts
+++ watermark/src/main.ts
@@ -4,31 +4,9 @@ import App from "./App.vue";
 import i18n from "./i18n";
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
-import * as Sentry from "@sentry/vue";
-import { BrowserTracing } from "@sentry/tracing";
 import router from "./router";
 
-const ENVIRONMENT = import.meta.env.VITE_ENVIRONMENT;
-
 const app = createApp(App);
-
-Sentry.init({
-  app,
-  dsn: "https://1e0bc34f71b74ed3b70019949edf107c@sentry.incubateur.net/113",
-  environment: ENVIRONMENT,
-  integrations: [
-    new BrowserTracing({
-      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-      tracingOrigins: [
-        "localhost",
-        "filigrane-dev.dossierfacile.fr",
-        "filigrane.beta.gouv.fr",
-        /^\//,
-      ],
-    }),
-  ],
-  tracesSampleRate: 0.01,
-});
 
 app.use(i18n);
 app.use(Toast as any);
