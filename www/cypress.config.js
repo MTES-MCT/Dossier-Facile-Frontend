c220acd4089b84fbfc8a3216d0f08b60a885c783
Matthieu Bollot <m.bollot@code-troopers.com>
2024-01-15 11:32:53 +0100

    chore: same github pb, other test

--- www/cypress.config.js
+++ www/cypress.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'http://127.0.0.1:4173'
-  }
+    baseUrl: 'http://localhost:4173'
+  },
+  hosts: { localhost: '127.0.0.1' }
 })
