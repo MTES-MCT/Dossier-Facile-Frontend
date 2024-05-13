dc9ddfc9fff49658d274e3c17353073d716343d0
Matthieu Bollot <m.bollot@code-troopers.com>
2024-05-13 16:03:27 +0200

    ✨ Feat(contact): add external links

--- df-shared-next/src/components/contact/OwnerHelpAccordion.vue
+++ df-shared-next/src/components/contact/OwnerHelpAccordion.vue
@@ -41,13 +41,13 @@ const elements = [
   "fr": {
     "title-1" : "Pourquoi ne puis-je pas consulter les dossiers envoyés par les candidats ?",
     "content-1": "Vous ne pouvez pas consulter les dossiers en cours de remplissage, en cours de traitement ou refusés. Vous ne pouvez consulter que les dossiers validés par notre équipe.",
-    "highlight-1": "Vous avez besoin de plus d’informations ? Consultez la FAQ de notre aide en ligne destinée aux propriétaires.",
+    "highlight-1": "Vous avez besoin de plus d’informations ? Consultez la <a href='https://docs.dossierfacile.logement.gouv.fr/category/125-faq-proprietaire' target='_blank'>FAQ</a> de notre <a href='https://docs.dossierfacile.logement.gouv.fr/' target='_blank'>aide en ligne</a> destinée aux propriétaires.",
     "title-2" : "Un candidat m’a envoyé son dossier validé mais je ne le vois pas sur le site ?",
-    "content-2": "Le candidat doit rejoindre votre espace propriétaire. Vous pouvez lui envoyer le lien vers notre page d’aide à ce sujet.",
-    "highlight-2": "Vous avez besoin de plus d’informations ? Consultez la FAQ de notre aide en ligne destinée aux propriétaires.",
+    "content-2": "Le candidat doit rejoindre votre espace propriétaire. Vous pouvez lui envoyer le lien vers <a href='https://dossierfacile.helpscoutdocs.com/article/39-candidater-au-bien-dun-proprietaire' target='_blank'>notre page d’aide à ce sujet</a>.",
+    "highlight-2": "Vous avez besoin de plus d’informations ? Consultez la <a href='https://docs.dossierfacile.logement.gouv.fr/category/125-faq-proprietaire' target='_blank'>FAQ</a> de notre <a href='https://docs.dossierfacile.logement.gouv.fr/' target='_blank'>aide en ligne</a> destinée aux propriétaires.",
     "title-3" : "Je n’arrive pas à me connecter à mon compte propriétaire.",
-    "content-3": "Rendez-vous dans l’espace propriétaire du site et cliquez sur le bouton se connecter en haut de la page.",
-    "highlight-3": "Votre adresse e-mail et votre mot de passe ne fonctionnent pas ? Nous vous recommandons de réinitialiser votre mot de passe.",
+    "content-3": "Rendez-vous dans l’<a href='https://proprietaire.dossierfacile.logement.gouv.fr/' target='_blank'>espace propriétaire</a> et connectez-vous. Vous pouvez laisser l’adresse e-mail et le mot de passe de votre espace propriétaire du site et cliquez sur le bouton <a href='https://proprietaire.dossierfacile.logement.gouv.fr/home' target='_blank'>se connecter</a> en haut de la page.",
+    "highlight-3": "Votre adresse e-mail et votre mot de passe ne fonctionnent pas ? Nous vous recommandons de <a href='https://proprietaire.dossierfacile.logement.gouv.fr/mot-de-passe-oublie' target='_blank'>réinitialiser votre mot de passe</a>.",
   }
 }
 </i18n>
--- df-shared-next/src/components/contact/TenantHelpAccordion.vue
+++ df-shared-next/src/components/contact/TenantHelpAccordion.vue
@@ -52,20 +52,20 @@ const elements = [
   },
   "fr": {
     "title-1" : "Pourquoi mon dossier a-t-il été refusé ?",
-    "content-1": "Notre équipe a examiné votre dossier et l’a refusé. Consultez nos commentaires dans votre messagerie et corrigez les erreurs indiquées.",
-    "highlight-1": "Vous avez besoin de plus d’informations ? Consultez la page Corriger un DossierFacile ou le tutoriel vidéo de notre aide en ligne.",
+    "content-1": "Notre équipe a examiné votre dossier et l’a refusé. Consultez nos commentaires dans <a href='https://locataire.dossierfacile.fr/messaging' target='_blank'>votre messagerie</a> et corrigez les erreurs indiquées.",
+    "highlight-1": "Vous avez besoin de plus d’informations ? Consultez la page <a href='https://docs.dossierfacile.logement.gouv.fr/article/34-les-modifications-demandees' target='_blank'>Corriger un DossierFacile</a> ou le <a href='https://docs.dossierfacile.logement.gouv.fr/article/34-les-modifications-demandees#Tutoriel-vido-_CaV2' target='_blank'>tutoriel vidéo</a> de notre <a href='https://docs.dossierfacile.logement.gouv.fr/' target='_blank'>aide en ligne</a>.",
     "title-2" : "Pourquoi mon dossier de colocation est-il incomplet alors qu’il a été validé ?",
     "content-2": "Votre dossier est validé, mais le dossier de votre colocataire (ou de vos colocataires) n’est pas validé. Votre dossier commun sera complet et partageable lorsque les dossiers de tous les colocataires seront validés.<br>Nous vous recommandons de contacter votre colocataire (ou vos colocataires) afin d’en savoir plus.",
-    "highlight-2": "Vous avez besoin de plus d’informations ? Consultez la page Constituer un dossier de location en famille ou entre amis de notre aide en ligne.",
+    "highlight-2": "Vous avez besoin de plus d’informations ? Consultez la page <a href='https://docs.dossierfacile.logement.gouv.fr/article/7-ajout-dun-colocataire' target='_blank'>Constituer un dossier de location en famille ou entre amis</a> de notre <a href='https://docs.dossierfacile.logement.gouv.fr/' target='_blank'>aide en ligne</a>.",
     "title-3" : "Comment envoyer mon DossierFacile à un propriétaire ?",
-    "content-3": "Pour envoyer votre DossierFacile à un propriétaire, rendez-vous sur la page Partager votre dossier. Si vous avez son adresse e-mail, vous pouvez lui envoyer directement par e-mail. Sinon, vous pouvez lui envoyer un simple lien vers votre DossierFacile.",
-    "highlight-3": "Vous avez besoin de plus d’informations ? Consultez la page Visualiser et partager son dossier validé de notre aide en ligne.",
+    "content-3": "Pour envoyer votre DossierFacile à un propriétaire, rendez-vous sur la page <a href='https://locataire.dossierfacile.logement.gouv.fr/applications' target='_blank'>Partager votre dossier</a>. Si vous avez son adresse e-mail, vous pouvez lui envoyer directement par e-mail. Sinon, vous pouvez lui envoyer un simple lien vers votre DossierFacile.",
+    "highlight-3": "Vous avez besoin de plus d’informations ? Consultez la page <a href='https://docs.dossierfacile.logement.gouv.fr/article/90-visualiserpartagerson-dossier-valide#Visualiser-son-dossier-complet-avec-les-pices-s9OWQ' target='_blank'>Visualiser et partager son dossier validé</a> de notre <a href='https://docs.dossierfacile.logement.gouv.fr/' target='_blank'>aide en ligne</a>.",
     "title-4" : "Comment créer un DossierFacile pour mon enfant ?",
     "content-4": "Un DossierFacile doit porter le nom de la personne qui signe le bail. Si votre enfant occupe le logement, le DossierFacile doit être à son nom.",
-    "highlight-4": "Vous avez utilisé votre compte FranceConnect pour créer le dossier de votre enfant, et notre équipe a refusé votre dossier ? Vous devez séparer vos comptes FranceConnect et DossierFacile. Consultez la page Séparer vos comptes FranceConnect et DossierFacile de notre aide en ligne.",
+    "highlight-4": "Vous avez utilisé votre compte FranceConnect pour créer le dossier de votre enfant, et notre équipe a refusé votre dossier ? Vous devez séparer vos comptes FranceConnect et DossierFacile. Consultez la page <a href='https://docs.dossierfacile.logement.gouv.fr/article/45-franceconnect#dissociation' target='_blank'>Séparer vos comptes FranceConnect et DossierFacile</a> de notre <a href='https://docs.dossierfacile.logement.gouv.fr/' target='_blank'>aide en ligne</a>.",
     "title-5" : "Où sont vos offres de logement en location ?",
     "content-5": "DossierFacile ne propose pas de logements. DossierFacile vous aide seulement à constituer un dossier de location numérique clair, complet et cohérent que vous pouvez ensuite envoyer à différents propriétaires.",
-    "highlight-5": "Vous avez besoin de plus d’informations ? Consultez la page DossierFacile c’est quoi ? de notre aide en ligne."
+    "highlight-5": "Vous avez besoin de plus d’informations ? Consultez la page <a href='https://dossierfacile.helpscoutdocs.com/article/28-dossierfacile-ne-propose-pas-de-logements' target='_blank'>DossierFacile c’est quoi</a> ? de notre <a href='https://docs.dossierfacile.logement.gouv.fr/' target='_blank'>aide en ligne</a>."
   }
 }
 </i18n>
