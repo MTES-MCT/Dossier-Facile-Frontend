c442bf0cbc5d8d67683f90af44189c3243f6a5df
Matthieu Bollot <m.bollot@code-troopers.com>
2024-05-13 16:55:40 +0200

    💄 UI(contact): update style for contact page on tenant and owner

--- owner/src/components/ContactPage.vue
+++ owner/src/components/ContactPage.vue
@@ -1,13 +1,21 @@
 <template>
-  <ContactForm :user="user" profile="owner" />
+  <div class="bg-blue">
+    <div class="fr-container">
+      <Breadcrumb :currentPage="t('contact.title')"/>
+      <ContactForm :user="user" profile="owner" />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import ContactForm from 'df-shared-next/src/components/ContactForm.vue';
+import Breadcrumb from "df-shared-next/src/components/dsfr/Breadcrumb.vue";
 import useOwnerStore from '../store/owner-store';
+import { useI18n } from 'vue-i18n';
 
 const store = useOwnerStore();
 const user = store.getUser;
+const { t } = useI18n();
 </script>
 
 <style scoped lang="scss"></style>
--- tenantv3/src/views/Contact.vue
+++ tenantv3/src/views/Contact.vue
@@ -1,14 +1,22 @@
 <template>
-  <ContactForm profile="tenant" :user="user" />
+  <div class="bg-blue">
+    <div class="fr-container">
+      <Breadcrumb :currentPage="t('contact.title')"/>
+      <ContactForm profile="tenant" :user="user" />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import ContactForm from "df-shared-next/src/components/ContactForm.vue";
+import Breadcrumb from "df-shared-next/src/components/dsfr/Breadcrumb.vue";
 import useTenantStore from "@/stores/tenant-store";
 import { computed, onBeforeUnmount, onMounted } from "vue";
+import { useI18n } from "vue-i18n";
 
 const store = useTenantStore();
 const user = computed(() => { return store.user });
+const { t } = useI18n();
 
   onMounted(() => {
     window.Beacon("init", "e9f4da7d-11be-4b40-9514-ac7ce3e68f67");
