1bb7e72771a71e9ff0a209f12fbe295cc8e13605
Matthieu Bollot <m.bollot@code-troopers.com>
2024-05-21 17:03:13 +0200

    ✨ Feat(contact): update wording (#1415)

--- df-shared-next/src/components/contact/OwnerHelpAccordion.vue
+++ df-shared-next/src/components/contact/OwnerHelpAccordion.vue
@@ -42,7 +42,7 @@ const elements = [
 {
   "en": {
     "title-1": "Why can't I view the files sent by candidates?",
-    "content-1": "You cannot view files that are being filled out, being processed, or have been rejected. You can only view files that have been validated by our team.",
+    "content-1": "You can only view files that have been validated by our team.<br>You cannot view files that are being filled out, being processed, or have been rejected.",
     "highlight-1": "Need more information? Check out the <a href='https://docs.dossierfacile.logement.gouv.fr/category/125-faq-proprietaire' target='_blank'>FAQ</a> in our <a href='https://docs.dossierfacile.logement.gouv.fr/' target='_blank'>online help</a> for landlords.",
     "title-2": "A candidate sent me their validated file but I can't see it on the site?",
     "content-2": "The candidate needs to join your landlord space. You can send them the link to <a href='https://dossierfacile.helpscoutdocs.com/article/39-candidater-au-bien-dun-proprietaire' target='_blank'>our help page on this topic</a>.",
@@ -53,7 +53,7 @@ const elements = [
   },
   "fr": {
     "title-1" : "Pourquoi ne puis-je pas consulter les dossiers envoyés par les candidats ?",
-    "content-1": "Vous ne pouvez pas consulter les dossiers en cours de remplissage, en cours de traitement ou refusés. Vous ne pouvez consulter que les dossiers validés par notre équipe.",
+    "content-1": "Vous ne pouvez consulter que les dossiers validés par notre équipe.<br>Vous ne pouvez pas consulter les dossiers en cours de remplissage, en cours de traitement ou refusés.",
     "highlight-1": "Vous avez besoin de plus d’informations ? Consultez la <a href='https://docs.dossierfacile.logement.gouv.fr/category/125-faq-proprietaire' target='_blank'>FAQ</a> de notre <a href='https://docs.dossierfacile.logement.gouv.fr/' target='_blank'>aide en ligne</a> destinée aux propriétaires.",
     "title-2" : "Un candidat m’a envoyé son dossier validé mais je ne le vois pas sur le site ?",
     "content-2": "Le candidat doit rejoindre votre espace propriétaire. Vous pouvez lui envoyer le lien vers <a href='https://dossierfacile.helpscoutdocs.com/article/39-candidater-au-bien-dun-proprietaire' target='_blank'>notre page d’aide à ce sujet</a>.",
