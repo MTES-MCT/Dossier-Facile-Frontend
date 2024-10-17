e107c51816c62d25bc7f4ee35b1495cb45637913
Jean-Marie Poissonnier <jean-marie@dossierfacile.fr>
2024-10-17 10:05:22 +0200

    🐛 Fix text not updating on language change

--- df-shared-next/src/components/contact/HelpAccordion.vue
+++ df-shared-next/src/components/contact/HelpAccordion.vue
@@ -1,36 +1,42 @@
 <template>
-    <div class="fr-accordions-group">
-        <section class="fr-accordion" v-for="e in elements" :key="e.id">
-            <h3 class="fr-accordion__title">
-                <button class="fr-accordion__btn fr-h3" @click="accordionClicked(e.tag)" aria-expanded="false" :aria-controls="e.id"><i class="circle ri-arrow-right-line fs-22"></i>{{ e.title }}</button>
-            </h3>
-            <div class="fr-collapse" :id="e.id">
-                <p v-html="e.content"></p>
-                <div class="fr-highlight">
-                    <p v-html="e.highlight"></p>
-                </div>
-            </div>
-        </section>
-    </div>
+  <div class="fr-accordions-group">
+    <section class="fr-accordion" v-for="e in elements" :key="e.id">
+      <h3 class="fr-accordion__title">
+        <button
+          class="fr-accordion__btn fr-h3"
+          @click="accordionClicked(e.tag)"
+          aria-expanded="false"
+          :aria-controls="e.id"
+        >
+          <i class="circle ri-arrow-right-line fs-22"></i>{{ e.title }}
+        </button>
+      </h3>
+      <div class="fr-collapse" :id="e.id">
+        <p v-html="e.content"></p>
+        <div class="fr-highlight">
+          <p v-html="e.highlight"></p>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { AccordionHelpModel } from '../../models/AccordionHelpModel';
 
-const props = defineProps<{
-    elements: AccordionHelpModel[],
+defineProps<{
+  elements: AccordionHelpModel[];
 }>();
 
-const emit = defineEmits(["accordion-clicked"]);
+const emit = defineEmits(['accordion-clicked']);
 
 function accordionClicked(tag: string) {
-    emit('accordion-clicked', tag)
+  emit('accordion-clicked', tag);
 }
-
 </script>
 
 <style scoped lang="scss">
 .fr-h3 {
-    font-weight: 400 !important;
+  font-weight: 400 !important;
 }
 </style>
