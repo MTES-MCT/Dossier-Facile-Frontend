c13041f23acac5a6058a1936a9222d1b6f16ff69
Jean-Marie Poissonnier <jean-marie@dossierfacile.fr>
2024-11-13 10:37:52 +0100

    ✨ Feat(crisp): open help page conditionally
    Only open help page if no session is ongoing

--- owner/src/plugin/crisp.ts
+++ owner/src/plugin/crisp.ts
@@ -2,7 +2,7 @@ import type { App } from 'vue'
 
 declare global {
   interface Window {
-    $crisp: unknown[]
+    $crisp: unknown[] & { is?: (method: string) => boolean }
     CRISP_WEBSITE_ID: string
   }
 }
@@ -11,7 +11,15 @@ export default {
   install(app: App, options: { websiteId: string }) {
     window.$crisp = [
       ['safe', true],
-      ['on', 'chat:opened', () => window.$crisp.push(['do', 'helpdesk:search'])]
+      [
+        'on',
+        'chat:opened',
+        () => {
+          if (!window.$crisp.is?.('session:ongoing')) {
+            window.$crisp.push(['do', 'helpdesk:search'])
+          }
+        }
+      ]
     ]
     window.CRISP_WEBSITE_ID = options.websiteId
 
