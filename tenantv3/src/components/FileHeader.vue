1a6081165692c3666aa8095b8a0f4bca2c0838fc
Jean-Marie Poissonnier <jean-marie@dossierfacile.fr>
2024-12-11 09:24:21 +0100

    ✨ Feat(tenant): Add explanation to shared file
    On shared file without supporting documents, add a text to explain.
    UI: Use full width for text

--- tenantv3/src/components/FileHeader.vue
+++ tenantv3/src/components/FileHeader.vue
@@ -1,19 +1,17 @@
 <template>
   <section class="background fr-pb-5w fr-mb-5w">
     <div class="fr-container">
-      <div class="fr-col-md-8">
-        <div class="fr-grid-col">
-          <p class="fr-badge fr-badge--sm fr-mt-3w">
-            {{ t('last-update', [getLastUpdateDate()]) }}
-          </p>
-          <h1 class="fr-h1 color--white fr-mt-3w" v-if="user">
-            {{ t('file.title', [getName()]) }}
-          </h1>
-          <p class="text-bold color--white">
-            {{ t('file.description', [getStatus(), getIncomeSum()]) }}
-          </p>
-          <slot></slot>
-        </div>
+      <div class="fr-grid-col">
+        <p class="fr-badge fr-badge--sm fr-mt-3w">
+          {{ t('last-update', [getLastUpdateDate()]) }}
+        </p>
+        <h1 class="fr-h1 color--white fr-mt-3w" v-if="user">
+          {{ t('file.title', [getName()]) }}
+        </h1>
+        <p class="text-bold color--white">
+          {{ t('file.description', [getStatus(), getIncomeSum()]) }}
+        </p>
+        <slot></slot>
       </div>
     </div>
   </section>
