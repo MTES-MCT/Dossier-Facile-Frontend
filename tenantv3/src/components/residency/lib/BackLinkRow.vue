ddea038517ca1f0e2ddd3475892d530835d5de9f
Jean-Marie Poissonnier <jean-marie@dossierfacile.fr>
2025-01-17 16:23:56 +0100

    💄 Feat: add checked icon

--- tenantv3/src/components/residency/lib/BackLinkRow.vue
+++ tenantv3/src/components/residency/lib/BackLinkRow.vue
@@ -1,5 +1,6 @@
 <template>
   <p class="display--flex align-items--center">
+    <RiCheckboxCircleLine color="var(--primary)" size="20px" class="fr-mr-1w no-shrink" />
     <span>{{ label }}</span>
     <router-link :to="to" class="fr-btn fr-btn--tertiary-no-outline fr-ml-auto"
       >Modifier <RiArrowGoBackLine size="1rem" class="fr-ml-1w"
@@ -9,7 +10,13 @@
 
 <script setup lang="ts">
 import type { RouterLinkProps } from 'vue-router'
-import { RiArrowGoBackLine } from '@remixicon/vue'
+import { RiArrowGoBackLine, RiCheckboxCircleLine } from '@remixicon/vue'
 
 defineProps<{ label: string; to: RouterLinkProps['to'] }>()
 </script>
+
+<style scoped>
+.no-shrink {
+  flex-shrink: 0;
+}
+</style>
