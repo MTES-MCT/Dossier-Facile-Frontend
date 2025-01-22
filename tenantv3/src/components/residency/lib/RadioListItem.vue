6b471cb1c5a1368cc19adc3c7f19cca0cce08112
Jean-Marie Poissonnier <jean-marie@dossierfacile.fr>
2025-01-22 16:43:15 +0100

    ♻️  Don't use unnecessary radio inputs

--- tenantv3/src/components/residency/lib/RadioListItem.vue
+++ tenantv3/src/components/residency/lib/RadioListItem.vue
@@ -1,16 +1,44 @@
 <template>
   <div class="fr-fieldset__element">
-    <router-link :to="to" class="fr-radio-group fr-radio-rich">
-      <input value="1" type="radio" :id="id" name="radio-rich-no-img" />
-      <label class="fr-label" :for="id"><slot /></label>
+    <router-link :to="to" class="radio-group">
+      <div class="radio-list-item">
+        <slot />
+      </div>
     </router-link>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useId } from 'vue'
 import type { RouterLinkProps } from 'vue-router'
 
 defineProps<{ to: RouterLinkProps['to'] }>()
-const id = useId()
 </script>
+
+<style scoped>
+.radio-list-item {
+  min-height: 5rem;
+  padding: 0.5rem 1.5rem 0.5rem 3.5rem;
+  border: 1px solid var(--border-default-grey);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: relative;
+
+  &:hover {
+    cursor: pointer;
+    background-color: var(--background-default-grey-hover);
+  }
+
+  &::after {
+    content: ' ';
+    background-color: transparent;
+    border: 1px solid var(--primary);
+    border-radius: 50%;
+    height: 1rem;
+    width: 1rem;
+    left: calc(2rem - 4px);
+    top: 2rem;
+    position: absolute;
+  }
+}
+</style>
