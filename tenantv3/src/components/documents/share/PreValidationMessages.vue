388935fb0ba928891ff658bfbd5b76747fd3b32a
Jean-Marie Poissonnier <jean-marie@dossierfacile.fr>
2025-01-30 15:10:08 +0100

    ✨ Feat: update prevalidation messages

--- tenantv3/src/components/documents/share/PreValidationMessages.vue
+++ tenantv3/src/components/documents/share/PreValidationMessages.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="rules-container" v-if="brokenRules && brokenRules.length > 0">
-    <h5 v-for="(b, k) in brokenRules" :key="k">
-      {{ b.message }}
-    </h5>
+    <template v-for="(b, k) in brokenRules" :key="k">
+      <h5>{{ b.message }}</h5>
+      <template v-if="isRuleWithCustomText(b.rule)">
+        <p>{{ t(`prevalidation.${b.rule}.p1`) }}</p>
+        <p>{{ t(`prevalidation.${b.rule}.p2`) }}</p>
+      </template>
+      <p v-else>{{ t('prevalidation.force-message') }}</p>
+    </template>
     <div class="form-container">
       <Form name="form" @submit="commentAnalysis">
-        <FieldLabel for-input="comment">{{ t('updatecomponent.force-message') }}</FieldLabel>
         <Field
           name="comment"
           v-model="comment"
@@ -14,7 +18,6 @@
         >
           <textarea
             v-bind="field"
-            id="comment"
             type="text"
             :value="comment"
             class="validate-required form-control fr-input"
@@ -41,7 +44,6 @@ import { useI18n } from 'vue-i18n'
 import useTenantStore from '../../../stores/tenant-store'
 import { Form, Field, ErrorMessage } from 'vee-validate'
 import DfButton from 'df-shared-next/src/Button/DfButton.vue'
-import FieldLabel from 'df-shared-next/src/components/form/FieldLabel.vue'
 import { ToastService } from '../../../services/ToastService'
 
 const { t } = useI18n()
@@ -73,6 +75,10 @@ function commentAnalysis() {
     ToastService.saveSuccess()
   })
 }
+
+const isRuleWithCustomText = (rule: string | undefined) =>
+  rule &&
+  ['R_RENT_RECEIPT_NAME', 'R_RENT_RECEIPT_MONTHS', 'R_RENT_RECEIPT_NB_DOCUMENTS'].includes(rule)
 </script>
 
 <style scoped>
@@ -83,3 +89,42 @@ function commentAnalysis() {
   padding: 1rem;
 }
 </style>
+
+<i18n>
+{
+  "fr": {
+    "prevalidation": {
+      "force-message": "Si vous pensez que notre outil fait erreur, vous pouvez expliquer le problème ci-dessous.",
+      "R_RENT_RECEIPT_NAME": {
+        "p1": "Notre outil ne trouve pas vos nom et prénom sur ce document.",
+        "p2": "Si notre outil fait erreur (par exemple, si vos quittances sont à votre nom de jeune fille), vous pouvez expliquer le problème ci-dessous."
+      },
+      "R_RENT_RECEIPT_NB_DOCUMENTS": {
+        "p1": "Notre outil ne détecte qu'un seul document. Avez-vous bien fourni vos 3 dernières quittances ?",
+        "p2": "Si notre outil fait erreur (par exemple, si vos 3 quittances sont réunies sur un seul document), vous pouvez expliquer le problème ci-dessous."
+      },
+      "R_RENT_RECEIPT_MONTHS": {
+        "p1": "Notre outil détecte des quittances trop anciennes.",
+        "p2": "Si notre outil fait erreur (par exemple, si une seule quittance correspond à plusieurs mois), vous pouvez expliquer le problème ci-dessous."
+      }
+    }
+  },
+  "en": {
+    "prevalidation": {
+      "force-message": "If you think our tool is making a mistake, you can explain the problem below",
+      "R_RENT_RECEIPT_NAME": {
+        "p1": "Our tool cannot find you first and last name on this document",
+        "p2": "If our tool makes a mistake (for example, if your receipts are in your maiden name), you can explain the problem below."
+      },
+      "R_RENT_RECEIPT_NB_DOCUMENTS": {
+        "p1": "Our tool detects only one document. Have you provided your last 3 receipts?",
+        "p2": "If our tool makes a mistake (for example, if your 3 receipts are combined in a single document), you can explain the problem below."
+      },
+      "R_RENT_RECEIPT_MONTHS": {
+        "p1": "Our tool detects receipts that are too old.",
+        "p2": "If our tool makes a mistake (for example, if a single receipt covers several months), you can explain the problem below."
+      }
+    }
+  }
+}
+</i18n>
