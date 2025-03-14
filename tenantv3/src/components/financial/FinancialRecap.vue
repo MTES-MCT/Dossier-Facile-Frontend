e68dbd83c84d6036f021358208f0adbdc5b4bf7b
Jean-Marie Poissonnier <jean-marie@dossierfacile.fr>
2025-03-14 11:12:50 +0100

    ✨ Feat: display financial incomes

--- tenantv3/src/components/financial/FinancialRecap.vue
+++ tenantv3/src/components/financial/FinancialRecap.vue
@@ -15,6 +15,24 @@
         <strong>notre équipe refuse automatiquement les montants surévalués</strong>.
       </p>
     </div>
+    <div class="fr-grid-col income-wrapper">
+      <div v-for="doc of store.tenantFinancialDocuments" :key="doc.id" class="income-card">
+        <div class="display--flex">
+          <span class="fr-text--lg bold">{{ t(`documents.${doc.documentType.key}`) }}</span>
+          <span class="fr-ml-auto">{{ doc.monthlySum }}€ net mensuel</span>
+        </div>
+        <div class="fr-ml-auto">
+          <router-link :to="makeLink(doc)" class="fr-link fr-mr-4w"
+            >Modifier
+            <RiEditLine size="1rem" />
+          </router-link>
+          <button type="button" class="btn-link color--primary" @click="deleteDoc(doc)">
+            Supprimer
+            <RiDeleteBinLine size="1rem" />
+          </button>
+        </div>
+      </div>
+    </div>
     <router-link to="4/ajouter" class="fr-btn fr-ml-auto fr-mt-3w"
       >Ajouter un revenu <RiAddFill class="tr-5"
     /></router-link>
@@ -25,11 +43,64 @@
 <script setup lang="ts">
 import NakedCard from 'df-shared-next/src/components/NakedCard.vue'
 import SimulationCaf from '../documents/share/SimulationCaf.vue'
-import { RiAddFill } from '@remixicon/vue'
+import { RiAddFill, RiDeleteBinLine, RiEditLine } from '@remixicon/vue'
+import { useTenantStore } from '@/stores/tenant-store'
+import { useI18n } from 'vue-i18n'
+import type { FinancialDocument } from 'df-shared-next/src/models/FinancialDocument'
+import { useLoading } from 'vue-loading-overlay'
+import { ToastService } from '@/services/ToastService'
+const store = useTenantStore()
+const { t } = useI18n()
+
+const STEP_ONE: Record<string, string> = {
+  SALARY: 'travail',
+  SOCIAL_SERVICE: 'social',
+  PENSION: 'pension',
+  RENT: 'rente',
+  SCHOLARSHIP: 'bourse',
+  NO_INCOME: 'pas-de-revenus'
+}
+
+function makeLink(doc: FinancialDocument) {
+  const cat = doc.documentType.value
+  if (!doc.id || !cat) {
+    return '/documents-locataire/4'
+  }
+  // const step = doc.documentCategoryStep
+  const link = `/documents-locataire/4/${doc.id}/${STEP_ONE[cat]}`
+  return link
+}
+
+function deleteDoc(f: FinancialDocument) {
+  if (f.id === undefined) {
+    return
+  }
+  const $loading = useLoading()
+  const loader = $loading.show()
+  store
+    .deleteDocument(f.id)
+    .catch(() => {
+      ToastService.error()
+    })
+    .finally(() => {
+      loader.hide()
+    })
+}
 </script>
 
 <style scoped>
 .tr-5 {
   translate: 5px;
+}
+.income-wrapper {
+  gap: 1rem;
+  margin-top: 1.5rem;
+}
+.income-card {
+  padding: 10px;
+  border-radius: 4px;
+  border: 1px solid #ddd;
+  display: flex;
+  flex-direction: column;
 }
 </style>
