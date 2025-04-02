dfb1d3ea5a8377578eb653fb4b37d7d777298d24
nicolasSagon <sagon.nicolas@gmail.com>
2025-04-02 09:54:13 +0200

    Feat: Track external documentation click

---
+++ tenantv3/src/components/identity/lib/TrackedExternalLink.vue
@@ -0,0 +1,18 @@
+<template>
+  <a :href="externalDoc.url" target="_blank" @click="onExternalLinkClicked">
+    <slot> </slot>
+  </a>
+</template>
+
+<script setup lang="ts">
+import { AnalyticsService } from '@/services/AnalyticsService'
+import type { ExternalDocumentation } from './ExternalDocumentation'
+
+const props = defineProps<{
+  externalDoc: ExternalDocumentation
+}>()
+
+const onExternalLinkClicked = () => {
+  AnalyticsService.tenantIdentityOpenExternalDoc(props.externalDoc)
+}
+</script>
