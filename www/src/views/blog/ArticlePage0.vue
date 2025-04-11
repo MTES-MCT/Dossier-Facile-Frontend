83dd6d97c815a422498cfd9c87d85f2b721fcffb
Jean-Marie Poissonnier <jean-marie@dossierfacile.fr>
2025-04-11 17:44:44 +0200

    🐛 Fix: bold is not a valid HTML tag

--- www/src/views/blog/ArticlePage0.vue
+++ www/src/views/blog/ArticlePage0.vue
@@ -4,7 +4,9 @@
       <div class="fr-container">
         <div class="fr-col-md-8">
           <div class="fr-grid-col">
-            <h1 class="fr-h1 color--white fr-mt-5w fr-mb-5w">Créez votre dossier de location en ligne avec DossierFacile</h1>
+            <h1 class="fr-h1 color--white fr-mt-5w fr-mb-5w">
+              Créez votre dossier de location en ligne avec DossierFacile
+            </h1>
             <p class="text-bold color--white">
               Votre dossier locatif numérique certifié par l'État, anciennement Locatio
             </p>
@@ -17,7 +19,9 @@
         <div class="fr-grid-row">
           <h2>Comment créer votre dossier de location d'appartement avec DossierFacile ?</h2>
           <p>
-            DossierFacile est le service public gratuit qui vous permet de constituer un dossier locataire numérique fiable et sécurisé. Notre plateforme vous accompagne pour créer un dossier de logement complet qui répond aux attentes des propriétaires.
+            DossierFacile est le service public gratuit qui vous permet de constituer un dossier
+            locataire numérique fiable et sécurisé. Notre plateforme vous accompagne pour créer un
+            dossier de logement complet qui répond aux attentes des propriétaires.
           </p>
           <ol>
             <li>
@@ -25,7 +29,12 @@
             </li>
 
             <p>
-              Accéder à <a href="https://www.dossierfacile.logement.gouv.fr">www.dossierfacile.logement.gouv.fr</a> pour constituer votre dossier locatif. Voici les pièces nécessaires pour un dossier de location complet&nbsp;:
+              Accéder à
+              <a href="https://www.dossierfacile.logement.gouv.fr"
+                >www.dossierfacile.logement.gouv.fr</a
+              >
+              pour constituer votre dossier locatif. Voici les pièces nécessaires pour un dossier de
+              location complet&nbsp;:
             </p>
 
             <ul>
@@ -35,23 +44,30 @@
               <li>Dernier avis d'imposition</li>
               <li>Justificatifs de ressources (3 derniers bulletins de salaire, bourses...)</li>
             </ul>
-            
-            <p>Ces documents constituent la base d'un dossier de location d'appartement conforme.</p>
-            
+
+            <p>
+              Ces documents constituent la base d'un dossier de location d'appartement conforme.
+            </p>
+
             <li>
               <h3>Validation de votre dossier locatif par nos experts</h3>
             </li>
             <p>
-              Notre équipe vérifie minutieusement votre dossier de location selon nos critères "3C" : 
-              <bold>Clair, Complet et Cohérent</bold>. Nous vous guidons pour optimiser votre dossier si nécessaire et appliquons un filigrane sur les pièces de votre dossier de location pour protéger votre dossier d'un usage non autorisé (usurpation d'identité, copie illégale...).
+              Notre équipe vérifie minutieusement votre dossier de location selon nos critères "3C"
+              :
+              <strong>Clair, Complet et Cohérent</strong>. Nous vous guidons pour optimiser votre
+              dossier si nécessaire et appliquons un filigrane sur les pièces de votre dossier de
+              location pour protéger votre dossier d'un usage non autorisé (usurpation d'identité,
+              copie illégale...).
             </p>
             <li>
               <h3>Partagez votre dossier de location certifié</h3>
             </li>
             <p>
-              Votre dossier locataire numérique est disponible sous deux formats : un lien URL sécurisé 
-              ou un fichier PDF. Les propriétaires et agents immobiliers peuvent consulter vos pièces 
-              justificatives en toute sécurité grâce au filigrane DossierFacile qui protège vos documents.
+              Votre dossier locataire numérique est disponible sous deux formats : un lien URL
+              sécurisé ou un fichier PDF. Les propriétaires et agents immobiliers peuvent consulter
+              vos pièces justificatives en toute sécurité grâce au filigrane DossierFacile qui
+              protège vos documents.
             </p>
           </ol>
           <p class="p-btn">
