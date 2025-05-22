45b6eee2185100628c88dd0fa9bc9776012e0b41
Jean-Marie Poissonnier <jean-marie@dossierfacile.fr>
2025-05-22 12:16:12 +0200

    ✅ Fix(www): landing page test

--- www/__tests__/LandingPage.spec.ts
+++ www/__tests__/LandingPage.spec.ts
@@ -1,15 +1,16 @@
-import { describe, it, expect, vi } from "vitest";
+import { describe, it, expect, vi } from 'vitest'
 import { mount } from '@vue/test-utils'
-import { useI18n } from "vue-i18n";
-import LandingPage from '@/views/LandingPage.vue';
-import axios from "axios";
+import { useI18n } from 'vue-i18n'
+import LandingPage from '@/views/LandingPage.vue'
+import axios from 'axios'
 
-vi.mock("vue-i18n");
+vi.mock('vue-i18n')
 vi.mock('axios')
+vi.mock('@unhead/vue')
 
 useI18n.mockReturnValue({
-  t: (tKey: string) => tKey,
-});
+  t: (tKey: string) => tKey
+})
 
 describe('Header', () => {
   it('renders properly', () => {
