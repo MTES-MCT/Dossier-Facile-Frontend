2ef96da75a44a0d287978924555042b04f5db32d
Jean-Marie Poissonnier <jean-marie@dossierfacile.fr>
2025-07-30 10:36:01 +0200

    ✨ Feat(tenant): Identity Other - Update text

--- tenantv3/src/components/identityDocument/IdentityOther.vue
+++ tenantv3/src/components/identityDocument/IdentityOther.vue
@@ -3,11 +3,7 @@
   <i18n-t :keypath="'add-valid-doc-' + textKey" tag="p"
     ><strong>{{ t('valid-doc') }}</strong></i18n-t
   >
-  <DsfrAlert type="info" small class="fr-mb-3w">
-    <i18n-t :keypath="'if-visa-' + textKey" tag="p">
-      <strong>{{ t('a-visa') }}</strong>
-    </i18n-t>
-  </DsfrAlert>
+  <InfoVisa />
   <UploadFilesIdentity category="OTHER_IDENTIFICATION" />
 </template>
 
@@ -15,9 +11,9 @@
 import { useI18n } from 'vue-i18n'
 import { useParentRoute } from '@/components/common/lib/useParentRoute'
 import BackLinkRow from './lib/IdentityBackRow.vue'
-import { DsfrAlert } from '@gouvminint/vue-dsfr'
 import UploadFilesIdentity from './lib/UploadFilesIdentity.vue'
 import { useIdentificationState } from './lib/identityDocumentState'
+import InfoVisa from './lib/InfoVisa.vue'
 
 const parent = useParentRoute()
 const { t } = useI18n()
@@ -33,11 +29,6 @@ const { textKey } = useIdentificationState()
     "add-valid-doc-couple": "Make sure your spouse's document is {0}.",
     "add-valid-doc-couple-guarantor": "Make sure their guarantor's document is {0}.",
     "valid-doc": "valid",
-    "if-visa-tenant": "If you hold {0}, be sure to enclose your valid passport.",
-    "if-visa-guarantor": "If your guarantor holds {0}, be sure to enclose their valid passport.",
-    "if-visa-couple": "If your spouse holds {0}, be sure to enclose their valid passport.",
-    "if-visa-couple-guarantor": "If their guarantor holds {0}, be sure to enclose their valid passport.",
-    "a-visa": "a visa"
   },
   "fr": {
     "other-doc": "Autre document",
@@ -46,11 +37,6 @@ const { textKey } = useIdentificationState()
     "add-valid-doc-couple": "Assurez-vous que le document de votre conjoint est {0}.",
     "add-valid-doc-couple-guarantor": "Assurez-vous que le document de son garant est {0}.",
     "valid-doc": "en cours de validité",
-    "if-visa-tenant": "Si vous êtes titulaire {0}, assurez-vous de joindre votre passeport en cours de validité.",
-    "if-visa-guarantor": "Si votre garant est titulaire {0}, assurez-vous de joindre son passeport en cours de validité.",
-    "if-visa-couple": "Si votre conjoint est titulaire {0}, assurez-vous de joindre son passeport en cours de validité.",
-    "if-visa-couple-guarantor": "Si son garant est titulaire {0}, assurez-vous de joindre son passeport en cours de validité.",
-    "a-visa": "d’un visa"
   }
 }
 </i18n>
--- tenantv3/src/components/identityDocument/IdentityPassport.vue
+++ tenantv3/src/components/identityDocument/IdentityPassport.vue
@@ -5,11 +5,7 @@
     <li class="bold">{{ t('valid') }}</li>
     <li class="bold">{{ t('pages-2-3') }}</li>
   </ul>
-  <DsfrAlert type="info" small class="fr-mb-3w">
-    <i18n-t :keypath="'if-visa-' + textKey" tag="p">
-      <strong>{{ t('a-visa') }}</strong>
-    </i18n-t>
-  </DsfrAlert>
+  <InfoVisa />
   <UploadFilesIdentity category="FRENCH_PASSPORT" />
 </template>
 
@@ -17,9 +13,9 @@
 import { useI18n } from 'vue-i18n'
 import { useParentRoute } from '@/components/common/lib/useParentRoute'
 import BackLinkRow from './lib/IdentityBackRow.vue'
-import { DsfrAlert } from '@gouvminint/vue-dsfr'
 import UploadFilesIdentity from './lib/UploadFilesIdentity.vue'
 import { useIdentificationState } from './lib/identityDocumentState'
+import InfoVisa from './lib/InfoVisa.vue'
 
 const parent = useParentRoute()
 const { t } = useI18n()
@@ -35,11 +31,6 @@ const { textKey } = useIdentificationState()
     "add-passport-couple-guarantor": "Be sure to enclose their guarantor's French or foreign passport:",
     "valid": "valid",
     "pages-2-3": "pages 2 and 3 to show signature",
-    "if-visa-tenant": "If you hold {0}, your must have a valid passport.",
-    "if-visa-guarantor": "If your guarantor holds {0}, their valid passport is mandatory.",
-    "if-visa-couple": "If your spouse holds {0}, their valid passport is mandatory.",
-    "if-visa-couple-guarantor": "If their guarantor holds {0}, their valid passport is mandatory.",
-    "a-visa": "a visa"
   },
   "fr": {
     "add-passport-tenant": "Assurez-vous de joindre votre passeport français ou étranger :",
@@ -48,11 +39,6 @@ const { textKey } = useIdentificationState()
     "add-passport-couple-guarantor": "Assurez-vous de joindre le passeport français ou étranger de son garant :",
     "valid": "en cours de validité",
     "pages-2-3": "les pages 2 et 3 pour faire apparaître la signature",
-    "if-visa-tenant": "Si vous êtes titulaire {0}, votre passeport en cours de validité est obligatoire.",
-    "if-visa-guarantor": "Si votre garant est titulaire {0}, son passeport en cours de validité est obligatoire.",
-    "if-visa-couple": "Si votre conjoint est titulaire {0}, son passeport en cours de validité est obligatoire.",
-    "if-visa-couple-guarantor": "Si son garant est titulaire {0}, son passeport en cours de validité est obligatoire.",
-    "a-visa": "d’un visa"
   }
 }
 </i18n>
