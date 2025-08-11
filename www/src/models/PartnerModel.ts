33e912f1413a795cbdd488dbbf347ea5dac6134b
Jean-Marie Poissonnier <jean-marie@dossierfacile.fr>
2025-08-11 16:10:05 +0200

    ✨ Feat(www): Update institutional partners

--- www/src/models/PartnerModel.ts
+++ www/src/models/PartnerModel.ts
@@ -2,7 +2,6 @@ import visaleUrl from '../assets/logos/visale.webp'
 import anilUrl from '../assets/logos/anil.webp'
 import unmlUrl from '../assets/logos/UNML.webp'
 import cllajUrl from '../assets/logos/CLLAJ.webp'
-import fuUrl from '../assets/logos/FranceUniversites.webp'
 
 import papUrl from '../assets/logos/pap.webp'
 import locserviceUrl from '../assets/logos/loc_service.webp'
@@ -35,6 +34,10 @@ import myappartUrl from '../assets/logos/myappart.png'
 import laforetUrl from '../assets/logos/laforet.webp'
 import ouestfranceImmoUrl from '../assets/logos/ouestfrance-immo.svg'
 import figaroImmoUrl from '../assets/logos/figaro-immobilier.svg'
+import cnousUrl from '../assets/logos/Crous.png'
+import dgesipUrl from '../assets/logos/dgesip.png'
+import servicePublicUrl from '../assets/logos/Service-public.fr.png'
+import ministereInterieurUrl from '../assets/logos/Ministère_de_l_Intérieur.png'
 
 interface Partner {
   name: string
@@ -46,18 +49,46 @@ interface Partner {
 
 export const INSTITUTIONAL_PARTNERS: Partner[] = [
   {
+    image: cnousUrl,
+    name: 'CNOUS',
+    href: 'https://www.lescrous.fr/les-crous/le-cnous/',
+    height: '120',
+    width: '120'
+  },
+  {
+    image: dgesipUrl,
+    name: 'DGESIP',
+    href: 'https://www.enseignementsup-recherche.gouv.fr/fr/direction-generale-de-l-enseignement-superieur-et-de-l-insertion-professionnelle-dgesip-83714',
+    height: '180',
+    width: '180'
+  },
+  {
+    image: anilUrl,
+    href: 'https://www.anil.org/votre-projet/vous-etes-locataire/se-loger/dans-le-prive/',
+    height: '80',
+    width: '80',
+    name: 'ANIL, l’agence nationale pour l’information sur le logement'
+  },
+  {
+    image: servicePublicUrl,
+    name: 'Service Public',
+    href: 'https://www.service-public.fr/particuliers/vosdroits/R51424',
+    height: '120',
+    width: '280'
+  },
+  {
+    image: ministereInterieurUrl,
+    name: 'Ministère de l’intérieur',
+    href: 'https://www.masecurite.interieur.gouv.fr/fr/fiches-pratiques/habitation/eviter-faux-dossiers-location-grace-dossierfacile?hl=dossierfacile',
+    height: '150',
+    width: '200'
+  },
+  {
     image: visaleUrl,
     height: '48',
     href: 'https://www.visale.fr/',
     name: 'visale.fr - connecte emploi et logement',
     width: '150'
-  },
-  {
-    image: anilUrl,
-    href: 'https://www.anil.org/',
-    height: '80',
-    width: '80',
-    name: 'ANIL, l’agence nationale pour l’information sur le logement'
   },
   {
     image: unmlUrl,
@@ -68,17 +99,10 @@ export const INSTITUTIONAL_PARTNERS: Partner[] = [
   },
   {
     image: cllajUrl,
-    href: 'https://www.uncllaj.org/',
+    href: 'https://www.projet-toit.fr/mon-dossier-de-location/',
     name: 'Comités Locaux pour le Logement Autonome des Jeunes',
     height: '60',
     width: '137'
-  },
-  {
-    image: fuUrl,
-    href: 'https://franceuniversites.fr/',
-    name: 'France Universités',
-    height: '60',
-    width: '194'
   }
 ]
 
