a91409de5b379c0e501275c3d5c24c5e9778a7b6
Jean-Marie Poissonnier <jean-marie@dossierfacile.fr>
2025-08-12 16:31:40 +0200

    🐛 Fix(www): logos aspect ratio

--- www/src/models/PartnerModel.ts
+++ www/src/models/PartnerModel.ts
@@ -60,7 +60,7 @@ export const INSTITUTIONAL_PARTNERS: Partner[] = [
     name: 'DGESIP',
     href: 'https://www.enseignementsup-recherche.gouv.fr/fr/direction-generale-de-l-enseignement-superieur-et-de-l-insertion-professionnelle-dgesip-83714',
     height: '180',
-    width: '180'
+    width: '220'
   },
   {
     image: anilUrl,
@@ -73,8 +73,8 @@ export const INSTITUTIONAL_PARTNERS: Partner[] = [
     image: servicePublicUrl,
     name: 'Service Public',
     href: 'https://www.service-public.fr/particuliers/vosdroits/R51424',
-    height: '120',
-    width: '280'
+    height: '93',
+    width: '270'
   },
   {
     image: ministereInterieurUrl,
