3efdc6fe93d40d04ac61f673295a8c746027cf29
Jean-Marie Poissonnier <jean-marie@dossierfacile.fr>
2025-09-15 11:34:29 +0200

    🐛 Fix: day of change for tax year
    'day' is day of week, 'date' is date of month

--- tenantv3/src/components/residency/lib/taxYear.ts
+++ tenantv3/src/components/residency/lib/taxYear.ts
@@ -2,5 +2,5 @@ import dayjs from 'dayjs'
 
 // Rule: current year if october 15th or later, else previous year
 const year = dayjs().year()
-const date = dayjs().set('year', year).set('month', 9).set('day', 15)
+const date = dayjs().set('year', year).set('month', 9).set('date', 15)
 export const taxYear = dayjs().isBefore(date) ? year - 1 : year
--- tenantv3/src/components/tax/lib/taxYear.ts
+++ tenantv3/src/components/tax/lib/taxYear.ts
@@ -2,5 +2,5 @@ import dayjs from 'dayjs'
 
 // Rule: current year if september 15th or later, else previous year
 const year = dayjs().year()
-const date = dayjs().set('year', year).set('month', 8).set('day', 15)
+const date = dayjs().set('year', year).set('month', 8).set('date', 15)
 export const taxYear = dayjs().isBefore(date) ? year - 1 : year
