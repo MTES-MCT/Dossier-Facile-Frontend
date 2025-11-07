e012c31af048b9b62dfe3768abf4714d61c50df3
Romaric Haldenwang <romaric@dossierfacile.fr>
2025-11-07 12:35:35 +0100

    chore: add robots.txt file in preprod environnement

---
+++ df-shared-next/src/plugin/robots.ts
@@ -0,0 +1,42 @@
+import { writeFile } from 'node:fs/promises'
+import { resolve } from 'node:path'
+
+import type { PluginOption } from 'vite'
+
+export type RobotsPluginOptions = {
+  environment?: string | null
+  disallowAllContent?: string[]
+}
+
+const defaultContent = ['User-agent: *', 'Disallow: /', '']
+
+export const createRobotsTxtPlugin = ({
+  // default to prod if not set
+  environment = process.env.VITE_ENVIRONMENT ?? 'prod',
+  disallowAllContent = defaultContent
+}: RobotsPluginOptions = {}): PluginOption => {
+  let outDir: string
+  let root: string
+
+  return {
+    name: 'environment-robots',
+    apply: 'build',
+    configResolved(config) {
+      outDir = config.build.outDir
+      root = config.root
+    },
+    async closeBundle() {
+      if (environment === 'prod') {
+        return
+      }
+
+      // overwrite a disallow-all robots.txt file for non-prod environments
+      const robotsPath = resolve(root, outDir, 'robots.txt')
+
+      await writeFile(robotsPath, disallowAllContent.join('\n'), 'utf8')
+    }
+  }
+}
+
+export default createRobotsTxtPlugin
+
