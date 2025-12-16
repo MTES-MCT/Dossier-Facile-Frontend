83e4b728d35b4ddc54236b3870d6ac04d37cf1b2
Romaric <Rhmaric@users.noreply.github.com>
2025-12-16 17:41:23 +0100

    feat(sharing-link): add trigram authentication (#1806)
    * feat: handle 403 and 429 api response statusCode
    
    * feat: add a modal screen for trigram authentication
    
    * refactor: reuse ModalComponent.vue
    
    * fix: SonarQube issues
    
    * feat: add logic for AB testing
    
    * feat: test if link exists before trigram auth
    
    * accessibility: handle focus on trigram auth input
    
    * tracking: add Matomo tracking

---
+++ tenantv3/src/views/FileTooManyRequest.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="fr-container">
+      <div class="fr-my-2w fr-grid-row fr-grid-row--gutters fr-grid-row--middle fr-grid-row--center">
+        <div class="fr-py-0 fr-col-12 fr-col-md-6">
+          <h1>{{ t('file.too-many-requests.title') }}</h1>
+          <p class="fr-text--lead">
+            {{ t('file.too-many-requests.line1') }}
+            <br />
+            {{ t('file.too-many-requests.line2') }}
+          </p>
+        </div>
+        <div class="fr-col-12 fr-col-md-3 fr-col-offset-md-1 fr-px-6w fr-px-md-0 fr-py-0">
+          <Artwork pictogram="connection-lost" />
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import Artwork from 'df-shared-next/src/components/ArtworkImage.vue'
+  import { useI18n } from 'vue-i18n'
+  const { t } = useI18n()
+  </script>
+  
