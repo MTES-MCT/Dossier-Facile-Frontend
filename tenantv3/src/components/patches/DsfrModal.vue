da177a4208bee99ca7cdc99c71262a3df8fe3680
nico <contact@nardu.in>
2026-01-21 10:35:51 +0100

    ♿ fix(tenant-form): accessibility issues on tenant parcours

---
+++ tenantv3/src/components/patches/DsfrModal.vue
@@ -0,0 +1,201 @@
+<script lang="ts" setup>
+/**
+ * This is a patched modal component from vue-dsfr.
+ * The original DsfrModal is not usable in this project.
+ * The 'opened' props is used to trigger a lot of things at once,
+ * which causes a delay in applying/updating properties here.
+ * This component is simplified and more accessible.
+ * I kept the original template for consistency with the doc.
+ */
+import type { DsfrModalProps } from '@gouvminint/vue-dsfr'
+
+import { useFocusTrap } from '@vueuse/integrations/useFocusTrap'
+import { computed, nextTick, onBeforeUnmount, onMounted, useTemplateRef, watch } from 'vue'
+
+import { DsfrButtonGroup } from '@gouvminint/vue-dsfr'
+import { VIcon } from '@gouvminint/vue-dsfr'
+
+import { useRandomId } from '@gouvminint/vue-dsfr'
+
+export type { DsfrModalProps }
+
+const props = withDefaults(defineProps<DsfrModalProps>(), {
+  modalId: () => useRandomId('modal', 'dialog'),
+  actions: () => [],
+  origin: () => ({ focus() {} }),
+  icon: undefined,
+  size: 'md',
+  closeButtonLabel: 'Fermer',
+  closeButtonTitle: 'Fermer la fenêtre modale'
+})
+
+const emit = defineEmits<{ (e: 'close'): void }>()
+
+defineSlots<{
+  /**
+   * Slot par défaut pour le contenu de la modale.
+   * Sera dans `<div class="fr-modal__content">`
+   */
+  default: () => any
+  /**
+   * Slot pour le pied-de-page de la modale.
+   * Sera dans `<div class="fr-modal__footer">`
+   */
+  footer?: () => any
+}>()
+
+const role = computed(() => {
+  return props.isAlert ? 'alertdialog' : 'dialog'
+})
+
+const modal = useTemplateRef('modal')
+
+const { activate, deactivate } = useFocusTrap(modal, {
+  immediate: false
+})
+
+watch(
+  () => props.opened,
+  async (newValue) => {
+    if (newValue) {
+      modal.value?.showModal()
+      setTimeout(() => {
+        activate()
+      }, 100)
+    } else {
+      deactivate()
+      modal.value?.close()
+    }
+    setAppropriateClassOnBody(newValue)
+  }
+)
+
+function setAppropriateClassOnBody(on: boolean) {
+  if (typeof window !== 'undefined') {
+    document.body.classList.toggle('modal-open', on)
+  }
+}
+
+onMounted(() => {
+  setAppropriateClassOnBody(props.opened)
+})
+
+onBeforeUnmount(() => {
+  setAppropriateClassOnBody(false)
+})
+
+async function close() {
+  await nextTick()
+  props.origin?.focus()
+  emit('close')
+}
+
+const dsfrIcon = computed(() => typeof props.icon === 'string' && props.icon.startsWith('fr-icon-'))
+const defaultScale = 2
+const iconProps = computed(() =>
+  dsfrIcon.value
+    ? undefined
+    : typeof props.icon === 'string'
+      ? { name: props.icon, scale: defaultScale }
+      : props.icon && typeof props.icon === 'object'
+        ? { scale: defaultScale, ...(props.icon as Record<string, any>) }
+        : undefined
+)
+</script>
+
+<template>
+  <dialog
+    id="fr-modal-1"
+    ref="modal"
+    :aria-labelledby="modalId"
+    :role="role"
+    class="fr-modal--patched"
+    @keydown.esc="close"
+  >
+    <div class="fr-container fr-container--fluid fr-container-md">
+      <div class="fr-grid-row fr-grid-row--center">
+        <div
+          class="fr-col-12"
+          :class="{
+            'fr-col-md-8': size === 'lg',
+            'fr-col-md-6': size === 'md',
+            'fr-col-md-4': size === 'sm'
+          }"
+        >
+          <div class="fr-modal__body">
+            <div class="fr-modal__header">
+              <button
+                ref="closeBtn"
+                autofocus
+                class="fr-btn fr-btn--close"
+                :title="closeButtonTitle"
+                aria-controls="fr-modal-1"
+                type="button"
+                @click="close()"
+              >
+                <span>
+                  {{ closeButtonLabel }}
+                </span>
+              </button>
+            </div>
+            <div class="fr-modal__content">
+              <h1 :id="modalId" class="fr-modal__title">
+                <span
+                  v-if="dsfrIcon || iconProps"
+                  :class="{
+                    [String(icon)]: dsfrIcon
+                  }"
+                >
+                  <VIcon v-if="icon && iconProps" v-bind="iconProps" />
+                </span>
+                {{ title }}
+              </h1>
+              <!-- @slot Slot par défaut pour le contenu de la liste. Sera dans `<ul class="fr-modal__title">` -->
+              <slot />
+            </div>
+            <div v-if="actions?.length || $slots.footer" class="fr-modal__footer">
+              <!-- @slot Slot pour le pied-de-page de la modale `<ul class="fr-modal__footer">` -->
+              <slot name="footer" />
+              <DsfrButtonGroup
+                v-if="actions?.length"
+                align="right"
+                :buttons="actions"
+                inline-layout-when="large"
+                reverse
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </dialog>
+</template>
+
+<style scoped>
+.fr-modal--patched {
+  --ground: 2000;
+  padding: 0;
+  color: var(--text-default-grey);
+  background-color: transparent;
+  border: none;
+  opacity: 0;
+  transition:
+    opacity 0.3s,
+    visibility 0.3s;
+  visibility: hidden;
+  z-index: 1750;
+}
+.fr-modal--patched[open] {
+  opacity: 1;
+  transition:
+    opacity 0.3s,
+    visibility 0.3s;
+  visibility: visible;
+}
+.fr-modal--patched::backdrop {
+  background-color: hsla(0, 0%, 9%, 0.64);
+}
+:global(body.modal-open) {
+  overflow: hidden;
+}
+</style>
