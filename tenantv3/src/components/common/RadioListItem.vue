19be8dd3cd28122a1b5e466e4b17e1596ce7c8e5
nico <contact@nardu.in>
2026-01-21 15:34:12 +0100

    ♿ fix(tenant-form): cotenant error handling

--- tenantv3/src/components/common/RadioListItem.vue
+++ tenantv3/src/components/common/RadioListItem.vue
@@ -1,44 +1,45 @@
 <template>
-  <div class="fr-fieldset__element">
-    <router-link :to="to" class="radio-group">
-      <div class="radio-list-item">
-        <slot />
-      </div>
+  <li>
+    <router-link :to="to" class="radio-list-item">
+      <slot />
     </router-link>
-  </div>
+
+    <!-- <DsfrTile :title :to :description :details :horizontal>
+      <template #header>
+        <slot name="header"></slot>
+      </template>
+    </DsfrTile> -->
+  </li>
 </template>
 
 <script setup lang="ts">
-import type { RouterLinkProps } from 'vue-router'
+import { DsfrTile } from '@gouvminint/vue-dsfr'
+import type { RouteLocationRaw, RouterLinkProps } from 'vue-router'
 
-defineProps<{ to: RouterLinkProps['to'] }>()
+interface Props {
+  to: RouterLinkProps['to']
+  // title: string
+  description?: string
+  details?: string
+  horizontal?: boolean
+}
+
+const { horizontal = true } = defineProps<Props>()
 </script>
 
 <style scoped>
 .radio-list-item {
-  min-height: 5rem;
-  padding: 0.5rem 1.5rem 0.5rem 3.5rem;
-  border: 1px solid var(--border-default-grey);
+  padding: 0.5rem 1.5rem 0.5rem;
   display: flex;
+  min-height: 5rem;
   flex-direction: column;
   justify-content: center;
-  position: relative;
-
-  &:hover {
-    cursor: pointer;
-    background-color: var(--background-default-grey-hover);
-  }
-
-  &::after {
-    content: ' ';
-    background-color: transparent;
-    border: 1px solid var(--primary);
-    border-radius: 50%;
-    height: 1rem;
-    width: 1rem;
-    left: calc(2rem - 4px);
-    top: 2rem;
-    position: absolute;
-  }
+  border: 1px solid var(--border-default-grey);
+  background-image: none;
+}
+.radio-list-item:hover {
+  cursor: pointer;
+  outline: 2px solid var(--background-action-high-blue-france-active);
+  background-color: var(--background-default-grey-hover);
 }
 </style>
