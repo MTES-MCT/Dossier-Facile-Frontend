a2786ba2fe4bc562f659e31e876312a92f15eff3
nicolasSagon <sagon.nicolas@gmail.com>
2026-01-21 09:19:33 +0100

    chore: update deps (#1812)
    * feat: update cypress version
    
    * feat: update axios
    
    * feat: override peer dependencies with cve : glob / systeminformation
    
    * feat: update vite version
    
    * feat: override js-yaml to correct a cve
    
    * feat: update npm-run-all2
    
    * feat: fix all deps versions
    
    * feat: whitelist postinstall script allowed
    
    * feat: add update cooldown on pnpm
    
    * fix : tenantv3 unit tests

--- tenantv3/src/__tests__/taxYear.spec.ts
+++ tenantv3/src/__tests__/taxYear.spec.ts
@@ -2,34 +2,36 @@ import { describe, expect, it } from 'vitest'
 import { getTaxYear } from '../components/tax/lib/taxYear'
 import { getTaxYear as getResidencyTaxYear } from '../components/residency/lib/taxYear'
 
+const year = new Date().getFullYear()
+
 describe('taxYear for tax', () => {
   it('Before 15/09 use the previous year', async () => {
-    expect(getTaxYear(new Date(2025, 4, 12))).to.eq(2024)
-    expect(getTaxYear(new Date(2025, 8, 14))).to.eq(2024)
+    expect(getTaxYear(new Date(year, 4, 12))).to.eq(year - 1)
+    expect(getTaxYear(new Date(year, 8, 14))).to.eq(year - 1)
   })
 
   it('On the 15/09 use the current year', async () => {
-    expect(getTaxYear(new Date(2025, 8, 15))).to.eq(2025)
+    expect(getTaxYear(new Date(year, 8, 15))).to.eq(year)
   })
 
   it('After 15/09 use the current year', async () => {
-    expect(getTaxYear(new Date(2025, 8, 16))).to.eq(2025)
-    expect(getTaxYear(new Date(2025, 10, 7))).to.eq(2025)
+    expect(getTaxYear(new Date(year, 8, 16))).to.eq(year)
+    expect(getTaxYear(new Date(year, 10, 7))).to.eq(year)
   })
 })
 
 describe('taxYear for residency', () => {
   it('Before 29/09 use the previous year', async () => {
-    expect(getResidencyTaxYear(new Date(2025, 4, 12))).to.eq(2024)
-    expect(getResidencyTaxYear(new Date(2025, 8, 28))).to.eq(2024)
+    expect(getResidencyTaxYear(new Date(year, 4, 12))).to.eq(year - 1)
+    expect(getResidencyTaxYear(new Date(year, 8, 28))).to.eq(year - 1)
   })
 
   it('On the 29/09 use the current year', async () => {
-    expect(getResidencyTaxYear(new Date(2025, 8, 29))).to.eq(2025)
+    expect(getResidencyTaxYear(new Date(year, 8, 29))).to.eq(year)
   })
 
   it('After 29/09 use the current year', async () => {
-    expect(getResidencyTaxYear(new Date(2025, 8, 30))).to.eq(2025)
-    expect(getResidencyTaxYear(new Date(2025, 10, 7))).to.eq(2025)
+    expect(getResidencyTaxYear(new Date(year, 8, 30))).to.eq(year)
+    expect(getResidencyTaxYear(new Date(year, 10, 7))).to.eq(year)
   })
 })
