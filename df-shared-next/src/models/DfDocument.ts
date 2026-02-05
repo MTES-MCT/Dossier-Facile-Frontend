14f057c493131753b23ebf93770467d7a203468d
nicolasSagon <sagon.nicolas@gmail.com>
2026-02-05 14:50:12 +0100

    feature: static review page design

--- df-shared-next/src/models/DfDocument.ts
+++ df-shared-next/src/models/DfDocument.ts
@@ -64,6 +64,40 @@ export type TaxCategoryStep =
 
 export type DocumentCategoryStep = ResidencyStep | FinancialStep | TaxCategoryStep
 
+export const STEP_LABEL: { [P in DocumentCategoryStep]?: string } = {
+  SALARY_EMPLOYED_LESS_3_MONTHS: 'documents.financial-steps.salary',
+  SALARY_EMPLOYED_MORE_3_MONTHS: 'documents.financial-steps.salary',
+  SALARY_EMPLOYED_NOT_YET: 'documents.financial-steps.salary',
+  SALARY_FREELANCE_AUTOENTREPRENEUR: 'documents.financial-steps.freelance',
+  SALARY_FREELANCE_OTHER: 'documents.financial-steps.freelance',
+  SALARY_INTERMITTENT: 'documents.financial-steps.intermitent',
+  SALARY_ARTIST_AUTHOR: 'documents.financial-steps.artist-author',
+  SALARY_UNKNOWN: 'documents.financial-steps.unknown',
+  SOCIAL_SERVICE_CAF_LESS_3_MONTHS: 'documents.financial-steps.caf-or-msa',
+  SOCIAL_SERVICE_CAF_MORE_3_MONTHS: 'documents.financial-steps.caf-or-msa',
+  SOCIAL_SERVICE_FRANCE_TRAVAIL_LESS_3_MONTHS: 'documents.financial-steps.france-travail',
+  SOCIAL_SERVICE_FRANCE_TRAVAIL_MORE_3_MONTHS: 'documents.financial-steps.france-travail',
+  SOCIAL_SERVICE_FRANCE_TRAVAIL_NOT_YET: 'documents.financial-steps.france-travail',
+  SOCIAL_SERVICE_APL_LESS_3_MONTHS: 'documents.financial-steps.apl',
+  SOCIAL_SERVICE_APL_MORE_3_MONTHS: 'documents.financial-steps.apl',
+  SOCIAL_SERVICE_APL_NOT_YET: 'documents.financial-steps.apl',
+  SOCIAL_SERVICE_AAH_LESS_3_MONTHS: 'documents.financial-steps.aah',
+  SOCIAL_SERVICE_AAH_MORE_3_MONTHS: 'documents.financial-steps.aah',
+  SOCIAL_SERVICE_AAH_NOT_YET: 'documents.financial-steps.aah',
+  SOCIAL_SERVICE_OTHER: 'documents.financial-steps.other-help',
+  PENSION_STATEMENT: 'documents.financial-steps.pension',
+  PENSION_NO_STATEMENT: 'documents.financial-steps.pension',
+  PENSION_DISABILITY_LESS_3_MONTHS: 'documents.financial-steps.disability-pension',
+  PENSION_DISABILITY_MORE_3_MONTHS: 'documents.financial-steps.disability-pension',
+  PENSION_DISABILITY_NOT_YET: 'documents.financial-steps.disability-pension',
+  PENSION_ALIMONY: 'documents.financial-steps.alimony-pension',
+  PENSION_UNKNOWN: 'documents.financial-steps.unknown-pension',
+  RENT_RENTAL_RECEIPT: 'documents.financial-steps.rent',
+  RENT_RENTAL_NO_RECEIPT: 'documents.financial-steps.rent',
+  RENT_ANNUITY_LIFE: 'documents.financial-steps.rent-annuity-life',
+  RENT_OTHER: 'documents.financial-steps.rent-other'
+}
+
 export class DfDocument {
   id?: number
   path?: string
