c3cc73e79a58995ad1fda1c2e7b37ce907834034
nicolasSagon <sagon.nicolas@gmail.com>
2026-02-05 14:50:12 +0100

    feature: static review page design

--- tenantv3/src/components/validateStep/AnalysisPreview.vue
+++ tenantv3/src/components/validateStep/AnalysisPreview.vue
@@ -1,6 +1,14 @@
 <template>
   <NakedCard class="fr-mt-3w">
-    <h1 class="fr-h6">Documents de {{ user.firstName }} {{ user.lastName }}</h1>
+    <p v-if="isTenant" class="fr-badge fr-badge--new fr-badge--no-icon fr-mb-1w">
+      {{ t('badge-loc') }}
+    </p>
+    <p v-if="!isTenant" class="fr-badge fr-badge--warning fr-badge--no-icon fr-mb-1w">
+      {{ t('badge-guarantor') }}
+    </p>
+    <h1 class="fr-h6">
+      {{ nameToDisplay }}
+    </h1>
     <DocumentPreviewCard
       v-for="doc in documents"
       :key="doc.document?.id || doc.documentCategory"
@@ -13,20 +21,57 @@
 <script setup lang="ts">
 import NakedCard from 'df-shared-next/src/components/NakedCard.vue'
 import { allTenantDocumentCategories, type DfDocument } from 'df-shared-next/src/models/DfDocument'
+import type { Guarantor } from 'df-shared-next/src/models/Guarantor'
 import type { DocumentAnalysisStatus, PreviewDocument, User } from 'df-shared-next/src/models/User'
 import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
 import DocumentPreviewCard from './DocumentPreviewCard.vue'
 
 const props = defineProps<{
-  user: User
+  user: User | Guarantor
   documentAnalysisStatus: DocumentAnalysisStatus[]
+  isTenant: boolean
 }>()
+
+const { t } = useI18n()
+
+const nameToDisplay = computed(() => {
+  if (
+    props.isTenant ||
+    (!props.isTenant &&
+      'typeGuarantor' in props.user &&
+      props.user.typeGuarantor === 'NATURAL_PERSON')
+  ) {
+    return t('documents-of', { firstName: props.user.firstName, lastName: props.user.lastName })
+  }
+  if (
+    !props.isTenant &&
+    'typeGuarantor' in props.user &&
+    props.user.typeGuarantor === 'LEGAL_PERSON'
+  ) {
+    return t('documents-of', { firstName: props.user.firstName, lastName: props.user.lastName })
+  }
+  if (!props.isTenant && 'typeGuarantor' in props.user && props.user.typeGuarantor === 'ORGANISM') {
+    return t('document-of-organization')
+  }
+  return t('documents-of')
+})
 
 const documents = computed(() => {
   const documentToReturn: PreviewDocument[] = []
   const userDocuments: DfDocument[] = props.user.documents || []
 
-  for (const category of allTenantDocumentCategories) {
+  let categories = allTenantDocumentCategories
+
+  if (!props.isTenant && 'typeGuarantor' in props.user) {
+    if (props.user.typeGuarantor === 'LEGAL_PERSON') {
+      categories = ['IDENTIFICATION_LEGAL_PERSON', 'IDENTIFICATION']
+    } else if (props.user.typeGuarantor === 'ORGANISM') {
+      categories = ['GUARANTEE_PROVIDER_CERTIFICATE']
+    }
+  }
+
+  for (const category of categories) {
     if (category === 'FINANCIAL') {
       const financialDocs = userDocuments.filter((d) => d.documentCategory === 'FINANCIAL')
       if (financialDocs.length > 0) {
@@ -60,3 +105,20 @@ const documents = computed(() => {
   return documentToReturn
 })
 </script>
+
+<i18n>
+{
+  "en": {
+    "documents-of": "Documents of {firstName} {lastName}",
+    "document-of-organization": "Documents of the organization",
+    "badge-loc": "TENANT FILE",
+    "badge-guarantor": "GUARANTOR FILE"
+  },
+  "fr": {
+    "documents-of": "Documents de {firstName} {lastName}",
+    "document-of-organization": "Documents de l'organisme",
+    "badge-loc": "DOSSIER LOCATAIRE",
+    "badge-guarantor": "DOSSIER GUARANT"
+  }
+}
+</i18n>
