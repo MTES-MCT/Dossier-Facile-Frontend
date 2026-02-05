55023b3a9d8e5b19d69ac9b62f7b4aa2a253247b
nicolasSagon <sagon.nicolas@gmail.com>
2026-02-05 14:50:12 +0100

    feature: static review page design

---
+++ tenantv3/src/components/validateStep/ErrorPreviewCard.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="fr-p-1w document-preview-card document-preview-card--error">
+    <div>
+      <p class="fr-badge fr-badge--warning fr-mb-1w">
+        {{ t('documents.status.to_correct') }}
+      </p>
+      <div class="fr-text--md fr-mb-1w">{{ label }}</div>
+      <p v-if="subTitle" class="fr-text--sm text-mention fr-mb-1w">{{ subTitle }}</p>
+      <hr class="fr-py-1w" />
+      <ul class="fr-mt-0 fr-mb-1w">
+        <li v-for="(rule, k) in failedRules" :key="k" class="fr-text--sm warning-text fr-my-0">
+          {{ rule.message }}
+        </li>
+      </ul>
+      <div class="fr-grid-row fr-grid-row--right">
+        <button class="fr-btn fr-btn--secondary" @click="goToEdit">
+          {{ t('documents.status.correct') }}
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { PreviewDocument } from 'df-shared-next/src/models/User'
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { useDocumentPreview } from './useDocumentPreview'
+
+const props = defineProps<{
+  previewDocument: PreviewDocument
+}>()
+
+const { t } = useI18n()
+const { label, subTitle, goToEdit } = useDocumentPreview(props.previewDocument)
+
+const failedRules = computed(() => {
+  return props.previewDocument.document?.documentAnalysisReport?.failedRules || []
+})
+</script>
+
+<style scoped lang="scss">
+.document-preview-card {
+  background-color: var(--background-default-grey);
+  border: 1px solid var(--border-default-grey);
+
+  &--error {
+    background-color: var(--background-default-grey);
+    border: 1px solid var(--red-marianne-main-472);
+  }
+}
+
+.separation {
+  color: var(--border-default-grey);
+}
+
+.warning-text {
+  color: var(--text-default-warning);
+}
+
+.text-mention {
+  color: var(--text-mention-grey);
+}
+</style>
+
+<i18n>
+{
+  "en": {
+    "documents": {
+      "status": {
+        "to_correct": "TO CORRECT",
+        "correct": "Correct"
+      }
+    }
+  },
+  "fr": {
+    "documents": {
+      "status": {
+        "to_correct": "À CORRIGER",
+        "correct": "Corriger"
+      }
+    }
+  }
+}
+</i18n>
---
+++ tenantv3/src/components/validateStep/MissingDocumentPreviewCard.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="fr-p-1w document-preview-card">
+    <p class="fr-badge fr-badge--info fr-mb-1w">{{ t('missing-document') }}</p>
+    <div class="document-preview-card__content">
+      <div class="file-name">
+        <p class="fr-text--md fr-mb-1w">{{ label }}</p>
+        <p v-if="subTitle" class="fr-text--sm text-mention fr-mb-1w">{{ subTitle }}</p>
+      </div>
+      <div class="actions fr-ml-auto">
+        <button class="fr-btn fr-btn--secondary fr-btn--sm" @click="goToEdit">
+          {{ t('add-document') }}
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { PreviewDocument } from 'df-shared-next/src/models/User'
+import { useDocumentPreview } from './useDocumentPreview'
+import { useI18n } from 'vue-i18n'
+
+const props = defineProps<{
+  previewDocument: PreviewDocument
+}>()
+
+const { t } = useI18n()
+
+const { label, subTitle, goToEdit } = useDocumentPreview(props.previewDocument)
+</script>
+
+<style scoped lang="scss">
+.document-preview-card {
+  background-color: var(--background-default-grey);
+  border: 1px solid var(--border-default-grey);
+
+  &__content {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+
+    @media (min-width: 768px) {
+      flex-direction: row;
+      align-items: center;
+    }
+  }
+}
+
+.file-name {
+  display: flex;
+  align-items: center;
+  width: 100%;
+
+  @media (min-width: 768px) {
+    width: auto;
+  }
+}
+
+.actions {
+  display: flex;
+  margin-top: 1rem;
+  @media (min-width: 768px) {
+    margin-top: 0;
+  }
+}
+
+.icon {
+  flex-shrink: 0;
+}
+
+.bleue {
+  color: var(--blue-france-sun-113-625);
+}
+</style>
+
+<i18n>
+{
+  "en": {
+    "add-document" : "Add a document",
+    "missing-document": "MISSING DOCUMENT"
+  },
+  "fr": {
+    "add-document" : "Ajouter un document",
+    "missing-document": "DOCUMENT MANQUANT"
+  }
+}
+</i18n>
