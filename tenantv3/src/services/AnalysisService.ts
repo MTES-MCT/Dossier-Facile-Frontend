e1a126cb152eae29d48572047509dd9653dd657d
nicolasSagon <sagon.nicolas@gmail.com>
2026-02-10 14:16:14 +0100

    wip: wire the data with the backend

---
+++ tenantv3/src/services/AnalysisService.ts
@@ -0,0 +1,28 @@
+import axios from 'axios'
+import type { DocumentAnalysisReport } from 'df-shared-next/src/models/DocumentAnalysisReport'
+
+export enum AnalysisStatus {
+  NO_ANALYSIS_SCHEDULED = 'NO_ANALYSIS_SCHEDULED',
+  IN_PROGRESS = 'IN_PROGRESS',
+  COMPLETED = 'COMPLETED'
+}
+
+export type DocumentAnalysisStatusDTO = {
+  documentId: number
+  status: AnalysisStatus
+  analyzedFiles?: number
+  totalFiles?: number
+  analysisReport?: DocumentAnalysisReport
+}
+
+export type ApplicationAnalysisResponse = {
+  listOfDocumentsAnalysisStatus: DocumentAnalysisStatusDTO[]
+}
+
+export const AnalysisService = {
+  getApplicationAnalysisStatus() {
+    return axios.get<ApplicationAnalysisResponse>(
+      `${import.meta.env.VITE_API_URL}/api/application/analysis-status`
+    )
+  }
+}
