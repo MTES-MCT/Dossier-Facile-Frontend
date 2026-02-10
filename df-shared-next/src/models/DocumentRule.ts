7a05f0843936e26561548eab715d3a97c8360f2a
nicolasSagon <sagon.nicolas@gmail.com>
2026-02-10 14:16:14 +0100

    wip: wire the data with the backend

--- df-shared-next/src/models/DocumentRule.ts
+++ df-shared-next/src/models/DocumentRule.ts
@@ -1,4 +1,47 @@
-export class DocumentRule {
-  rule?: string
-  message?: string
+export type Name = {
+  firstNames: string
+  lastName: string
+  preferredName: string | null
+}
+
+export type FrenchIdentityCardExpirationRuleData = {
+  type: 'R_FRENCH_IDENTITY_CARD_EXPIRATION'
+  extractedDate: string
+}
+
+export type TaxClassificationRuleData = {
+  type: 'R_TAX_CLASSIFICATION'
+  isDeclarativeSituation: boolean
+}
+
+export type NamesRuleData = {
+  type: 'R_NAMES'
+  expectedName: Name
+  extractedNames: Name[]
+}
+
+export type TaxYearsRuleData = {
+  type: 'R_TAX_YEARS'
+  expectedYear: number
+  extractedYears: number[]
+}
+
+export type PayslipContinuityRuleData = {
+  type: 'R_PAYSLIP_CONTINUITY'
+  expectedMonthList: string[]
+  extractedMonthList: string[]
+}
+
+export type RuleData =
+  | FrenchIdentityCardExpirationRuleData
+  | TaxClassificationRuleData
+  | NamesRuleData
+  | TaxYearsRuleData
+  | PayslipContinuityRuleData
+
+export type DocumentRule = {
+  rule: string
+  message: string
+  level: string
+  ruleData: RuleData | null
 }
