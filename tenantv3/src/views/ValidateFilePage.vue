199fe9b4184db2f2b133fb9d4eec91007d380d2c
nicolasSagon <sagon.nicolas@gmail.com>
2026-02-12 17:27:15 +0100

    feat: wire the preview button

--- tenantv3/src/views/ValidateFilePage.vue
+++ tenantv3/src/views/ValidateFilePage.vue
@@ -1,6 +1,18 @@
 <template>
   <ProfileContainer :step="getStep()">
     <ValidateFile @on-back="goBack"></ValidateFile>
+    <div class="contact-support-container">
+      <RiQuestionFill class="icon" aria-hidden="true" />
+      <a
+        class="contact-link"
+        href="/contact"
+        target="_blank"
+        rel="noopener noreferrer"
+        :title="`${t('trigram.support-link')} - ${t('new-window')}`"
+      >
+        {{ t('trigram.support-link') }}
+      </a>
+    </div>
   </ProfileContainer>
 </template>
 
@@ -10,10 +22,14 @@ import ProfileContainer from '../components/ProfileContainer.vue'
 import { useTenantStore } from '@/stores/tenant-store'
 import { computed } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
+import { useI18n } from 'vue-i18n'
+import { RiQuestionFill } from '@remixicon/vue'
 
 const store = useTenantStore()
 const user = computed(() => store.user)
 const coTenants = computed(() => store.coTenants)
+
+const { t } = useI18n()
 
 const route = useRoute()
 const router = useRouter()
@@ -45,3 +61,24 @@ function goBack() {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.contact-support-container {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  margin-top: 1rem;
+}
+
+.icon {
+  color: var(--primary);
+  width: 1.5rem;
+  height: 1.5rem;
+  margin-right: 0.5rem;
+}
+
+.contact-link {
+  color: var(--primary);
+  font-size: 1rem;
+}
+</style>
