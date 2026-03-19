f8f946fad50e20984faecd194375ad1a184a3b26
nico <contact@nardu.in>
2026-03-19 18:19:38 +0100

    ✨ feat(footer-form): WIP add validation to file upload

--- tenantv3/src/components/uploads/FileUpload.vue
+++ tenantv3/src/components/uploads/FileUpload.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="file-upload fr-col-md-12">
+  <div class="file-upload-group">
     <form
       id="uploadForm"
       ref="uploadForm"
@@ -7,36 +7,34 @@
       enctype="multipart/form-data"
       novalidate
     >
-      <label for="file" class="visually-hidden">Ajouter un document</label>
       <div class="dropbox">
+        <p v-if="isSaving">{{ t('fileupload.uploading-files') }}</p>
+
+        <label for="file" class="fr-mb-3v"
+          >{{ t('fileupload.drag-and-drop-files') }} {{ t('fileupload.browse-files') }}
+          <span class="fr-hint-text">
+            {{ t('fileupload.files-format') }}<br />
+            {{ sizeLimit }}<br v-if="sizeLimit" />
+            {{ pagesLimit }}
+          </span>
+        </label>
         <input
           id="file"
           ref="inputFile"
           type="file"
           multiple
           :disabled="isSaving"
-          class="input-file"
+          aria-describedby="errors-file-upload"
+          class="input-file fr-upload fr-mt-1w"
           accept="image/png, image/jpeg, image/heic, application/pdf"
-          @change="filesChange"
+          @change="onFileChange"
         />
-        <div v-if="isSaving">
-          {{ t('fileupload.uploading-files') }}
-        </div>
-        <div v-else>
-          <p class="fr-mb-3v">{{ t('fileupload.drag-and-drop-files') }}</p>
-          <p class="text-small text-grey fr-mb-3v">
-            {{ t('fileupload.files-format') }}<br />
-            {{ sizeLimit }}<br v-if="sizeLimit" />
-            {{ pagesLimit }}
-          </p>
-          <p class="fr-mb-1w">
-            {{ t('fileupload.browse-files') }}
-            <label for="file" class="label-btn">
-              {{ t('fileupload.browse') }}
-            </label>
-          </p>
-        </div>
       </div>
+      <FieldErrors
+        v-if="r$.$errors.files"
+        id="errors-file-upload"
+        :errors="flatErrors(r$.$errors.files)"
+      />
     </form>
   </div>
 </template>
@@ -46,6 +44,9 @@ import { UploadStatus } from 'df-shared-next/src/models/UploadStatus'
 import { computed, useTemplateRef } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { toast } from '@/components/toast/toastUtils'
+import { useRegle, flatErrors } from '@regle/core'
+import { exactLength, fileType, maxFileSize, withMessage } from '@regle/rules'
+import FieldErrors from 'df-shared-next/src/components/form/FieldErrors.vue'
 
 const { t } = useI18n()
 const emit = defineEmits<{ 'add-files': [files: File[]] }>()
@@ -59,35 +60,46 @@ const props = withDefaults(
   defineProps<{ currentStatus?: number; page?: number; size?: number }>(),
   {
     currentStatus: UploadStatus.STATUS_INITIAL,
-    page: 0,
+    page: 1,
     size: 10
   }
 )
 
+const ALLOWED_FILE_TYPES = ['image/png', 'image/jpeg', 'application/pdf', 'image/heif']
+
+const sizeInMb: number = props.size * 1024 * 1024
+
+/**
+ * TODO:
+ * custom error messages for type and size
+ * pass the current saved files as an initial value for the r$ files
+ */
+const { r$ } = useRegle(
+  { files: [] as File[] },
+  {
+    files: {
+      fileType: fileType(ALLOWED_FILE_TYPES),
+      maxFileSize: maxFileSize(sizeInMb),
+      exactLength: withMessage(exactLength(props.page), ({ $value, $params: [max] }) =>
+        t('validation.exactFile', { max, n: $value?.length })
+      )
+    }
+  }
+)
+
+const onFileChange = async (event: Event) => {
+  const input = event.target as HTMLInputElement
+  r$.$value.files = input.files ? Array.from(input.files) : []
+
+  const { valid } = await r$.$validate()
+
+  if (!valid) return
+  emit('add-files', r$.$value.files)
+}
+
 const isSaving = computed(() => props.currentStatus === UploadStatus.STATUS_SAVING)
 const sizeLimit = computed(() => (props.size > 0 ? t('fileupload.size', [props.size]) : ''))
 const pagesLimit = computed(() => (props.page > 0 ? t('fileupload.pages', [props.page]) : ''))
-
-const ALLOWED_FILE_TYPES = ['image/png', 'image/jpeg', 'application/pdf', 'image/heif']
-
-const isTooBig = (file: File) => file.size > props.size * 1024 * 1024
-const hasValidExtension = (file: File) => ALLOWED_FILE_TYPES.includes(file.type)
-
-function filesChange(e: Event) {
-  const inputFiles = e.target instanceof HTMLInputElement ? e.target.files : null
-  const files = inputFiles ? [...inputFiles] : []
-  if (files.some(isTooBig)) {
-    toast.error(t('fileupload.file-too-big', [props.size]), inputFile.value)
-  }
-  if (!files.every(hasValidExtension)) {
-    toast.error(t('errors.invalid-file-extension'), inputFile.value)
-  }
-  const fileList = files.filter((f) => !isTooBig(f) && hasValidExtension(f))
-  if (fileList.length > 0) {
-    emit('add-files', fileList)
-  }
-  uploadForm.value?.reset()
-}
 </script>
 
 <style scoped lang="scss">
@@ -99,7 +111,14 @@ function filesChange(e: Event) {
 }
 
 .input-file {
-  opacity: 0;
+  position: static;
+  // padding: 0.5rem 1rem;
+  // margin-left: 0.5rem;
+  // border-radius: 4px;
+  // background-color: var(--background-contrast-grey);
+}
+.input-file::after {
+  content: '';
   position: absolute;
   inset: 0;
 }
@@ -109,18 +128,5 @@ function filesChange(e: Event) {
   outline-width: 2px;
   outline-offset: 4px;
   outline-color: #0a76f6;
-}
-
-.text-small {
-  font-size: 0.75rem;
-  line-height: 1.25rem;
-}
-
-.label-btn {
-  padding: 4px 8px;
-  margin-left: 0.5rem;
-  border-radius: 4px;
-  background-color: var(--background-contrast-grey);
-  cursor: pointer;
 }
 </style>
