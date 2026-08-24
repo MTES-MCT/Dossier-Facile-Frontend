a5e69b524133da0ca8eb7aafa90d9b0263fa7153
nicolasSagon <sagon.nicolas@gmail.com>
2026-08-24 11:25:30 +0200

    fix: new visale document ia design qa

--- df-shared-next/src/models/DocumentAnalysisReport.ts
+++ df-shared-next/src/models/DocumentAnalysisReport.ts
@@ -2,7 +2,7 @@ import type { DocumentRule } from './DocumentRule'
 
 export type DocumentAnalysisReport = {
   id: number
-  analysisStatus: 'DENIED' | 'CHECKED' | 'VALIDATED'
+  analysisStatus: 'DENIED' | 'CHECKED' | 'UNDEFINED'
   failedRules: DocumentRule[]
   passedRules: DocumentRule[]
   inconclusiveRules: DocumentRule[]
